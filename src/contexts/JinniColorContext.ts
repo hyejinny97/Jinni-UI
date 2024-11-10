35ed4c411889cbb4be64d2f98a9fe78947c93991
hyejin <yhj970102@gmail.com>
2024-11-10 14:33:18 +0900

    ➖ Remove: JinniColorContext 및 관련 파일 전부 삭제

--- src/contexts/JinniColorContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-import type { JinniColorType } from '@/components/JinniColorProvider';
-
-export const JinniColorContext = createContext<JinniColorType | null>(null);
