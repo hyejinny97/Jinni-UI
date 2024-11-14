e4d1bc6e9b140dc608b2e0aa15efd73321ecc24a
hyejin <yhj970102@gmail.com>
2024-11-14 17:29:51 +0900

    💄 Design:  typography 작성

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '@/styles/color.scss';
+import '@/styles/typography.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
