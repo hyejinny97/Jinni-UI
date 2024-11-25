e82adc8d00df027652f69cfe8a3d82e50f709542
hyejin <yhj970102@gmail.com>
2024-11-25 17:38:16 +0900

    ♻️ Refactor :  getJinniColorValue 함수 파라미터명 변경

--- src/utils/editColorStyle.ts
+++ src/utils/editColorStyle.ts
@@ -1,14 +1,12 @@
 import { CSS_COLOR_PROPERTIES } from '@/constants/css-color-properties';
 
-const getJinniColorValue = (colorVariable: string) => {
+const getJinniColorValue = (color: string) => {
   const rootEl = document.querySelector(':root');
   if (!rootEl) throw Error('root element를 가져오지 못함');
 
   const rootStyles = window.getComputedStyle(rootEl);
-  const colorValue = rootStyles.getPropertyValue(
-    `--jinni-color-${colorVariable}`
-  );
-  if (!colorValue) throw Error(`${colorValue} 색상은 존재하지 않습니다`);
+  const colorValue = rootStyles.getPropertyValue(`--jinni-color-${color}`);
+  if (!colorValue) throw Error(`'${color}' 색상은 존재하지 않습니다`);
 
   return colorValue;
 };
