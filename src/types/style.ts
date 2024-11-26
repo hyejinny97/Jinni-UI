ad359505aabedc453cdd8d1d9173cd44ca1cdd0c
hyejin <yhj970102@gmail.com>
2024-11-26 16:20:14 +0900

    ♻️ Refactor :  StyleType 선언 코드 단순화

--- src/types/style.ts
+++ src/types/style.ts
@@ -3,14 +3,13 @@ import { CSS_COLOR_PROPERTIES } from '@/constants/css-color-properties';
 import { Responsive } from '@/types/breakpoint';
 
 type CSSColorProperties = (typeof CSS_COLOR_PROPERTIES)[number];
-type ResponsiveCSSProperties = {
-  [K in keyof React.CSSProperties]?:
-    | React.CSSProperties[K]
-    | Responsive<React.CSSProperties[K]>;
-};
-type ResponsiveCSSColorProperties = Partial<
-  Record<CSSColorProperties, ColorType | Responsive<ColorType>>
->;
 
-export type StyleType = Omit<ResponsiveCSSProperties, CSSColorProperties> &
-  ResponsiveCSSColorProperties;
+type BaseCSSProperties = Omit<React.CSSProperties, CSSColorProperties> &
+  Partial<Record<CSSColorProperties, ColorType>>;
+type ResponsiveCSSProperties = {
+  [K in keyof BaseCSSProperties]?:
+    | BaseCSSProperties[K]
+    | Responsive<BaseCSSProperties[K]>;
+};
+
+export type StyleType = ResponsiveCSSProperties;
