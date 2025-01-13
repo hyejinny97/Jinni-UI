4dfad7ae76db2f546074d80e3cc708ca5deae097
hyejin <yhj970102@gmail.com>
2025-01-13 17:16:27 +0900

    ✨ Feat :  `Alert` 컴포넌트 구현

---
+++ src/components/feedback/Alert/Alert.tsx
@@ -0,0 +1,54 @@
+import './Alert.scss';
+import cn from 'classnames';
+import useStyle from '@/hooks/useStyle';
+import { AsType, DefaultComponentProps } from '@/types/default-component-props';
+import { getDefaultIconBySeverity } from './Alert.utills';
+
+export type SeverityType = 'success' | 'info' | 'warning' | 'error';
+
+type AlertProps<T extends AsType = 'div'> = DefaultComponentProps<T> & {
+  children: React.ReactNode;
+  severity?: SeverityType;
+  variant?: 'subtle-filled' | 'filled' | 'outlined';
+  icon?: React.ReactNode;
+  action?: React.ReactNode;
+  title?: React.ReactNode;
+};
+
+const Alert = <T extends AsType = 'div'>(props: AlertProps<T>) => {
+  const {
+    children,
+    severity = 'success',
+    variant = 'subtle-filled',
+    icon,
+    action,
+    title,
+    className,
+    style,
+    as: Component = 'div',
+    ...rest
+  } = props;
+  const hasTitle = !!title;
+  const isDefaultIcon = !icon;
+  const computedIcon = icon || getDefaultIconBySeverity({ severity });
+  const newStyle = useStyle(style);
+
+  return (
+    <Component
+      className={cn('JinniAlert', severity, variant, className)}
+      style={newStyle}
+      {...rest}
+    >
+      <div className={cn('JinniAlertIcon', { isDefaultIcon })}>
+        {computedIcon}
+      </div>
+      <div className="JinniAlertMessage">
+        {hasTitle && <div className="JinniAlertMessageTitle">{title}</div>}
+        {children}
+      </div>
+      <div className="JinniAlertAction">{action}</div>
+    </Component>
+  );
+};
+
+export default Alert;
