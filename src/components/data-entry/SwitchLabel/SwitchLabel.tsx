2d0dceee9794c6c868b7fbec77a9a9e272878c67
hyejin <yhj970102@gmail.com>
2025-02-20 00:14:28 +0900

    ✨ Feat :  `Switch`, `SwitchLabel` 컴포넌트 구현

---
+++ src/components/data-entry/SwitchLabel/SwitchLabel.tsx
@@ -0,0 +1,6 @@
+import { FormControlLabel } from '@/components/_share/FormControlLabel';
+import { SwitchProps } from '@/components/data-entry/Switch';
+
+const SwitchLabel = FormControlLabel<SwitchProps>;
+
+export default SwitchLabel;
