e44afc24da3e961ff75522c1b1bdccd60fac0b01
hyejin <yhj970102@gmail.com>
2025-04-24 16:31:33 +0900

    ✨ Feat :  `List` 컴포넌트 구현

---
+++ src/components/data-display/List/ListItem.tsx
@@ -0,0 +1,24 @@
+import cn from 'classnames';
+import { AsType, DefaultComponentProps } from '@/types/default-component-props';
+import useStyle from '@/hooks/useStyle';
+
+type ListItemProps<T extends AsType = 'div'> = DefaultComponentProps<T> & {
+  children: React.ReactNode;
+};
+
+const ListItem = <T extends AsType = 'div'>(props: ListItemProps<T>) => {
+  const { children, className, style, as: Component = 'div', ...rest } = props;
+  const newStyle = useStyle(style);
+
+  return (
+    <Component
+      className={cn('JinniListItem', className)}
+      style={newStyle}
+      {...rest}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export default ListItem;
