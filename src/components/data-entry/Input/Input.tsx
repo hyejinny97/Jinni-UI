b93452da9a10306ab1631c81c33f7c77fbe95ad4
hyejin <yhj970102@gmail.com>
2025-05-20 16:41:46 +0900

    ♻️ Refactor :  `Input` - InputBase (version2)에 따라 수정

--- src/components/data-entry/Input/Input.tsx
+++ src/components/data-entry/Input/Input.tsx
@@ -1,26 +1,77 @@
 import cn from 'classnames';
 import { InputBase, InputBaseProps } from '@/components/data-entry/InputBase';
+import { DefaultComponentProps } from '@/types/default-component-props';
+import { useInputValue } from './Input.hooks';
 
-export type InputProps = InputBaseProps<'input'> & {
-  type?:
-    | 'date'
-    | 'datetime-local'
-    | 'email'
-    | 'month'
-    | 'number'
-    | 'password'
-    | 'search'
-    | 'tel'
-    | 'text'
-    | 'time'
-    | 'url'
-    | 'week';
-};
+export type InputProps = Omit<DefaultComponentProps<'input'>, 'size'> &
+  InputBaseProps & {
+    type?:
+      | 'date'
+      | 'datetime-local'
+      | 'email'
+      | 'month'
+      | 'number'
+      | 'password'
+      | 'search'
+      | 'tel'
+      | 'text'
+      | 'time'
+      | 'url'
+      | 'week';
+    defaultValue?: string | number;
+    value?: string | number;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    disabled?: boolean;
+  };
 
 const Input = (props: InputProps) => {
-  const { className, ...rest } = props;
+  const {
+    type = 'text',
+    defaultValue,
+    value,
+    onChange,
+    disabled,
+    startAdornment,
+    endAdornment,
+    variant,
+    size,
+    color,
+    disableHoverEffect,
+    disableFocusEffect,
+    fullWidth,
+    className,
+    style,
+    ...rest
+  } = props;
+  const { inputValue, handleChange } = useInputValue({
+    defaultValue,
+    value,
+    onChange
+  });
 
-  return <InputBase className={cn('JinniInput', className)} {...rest} />;
+  return (
+    <InputBase
+      className={cn('JinniInput', className)}
+      style={style}
+      disabled={disabled}
+      startAdornment={startAdornment}
+      endAdornment={endAdornment}
+      variant={variant}
+      size={size}
+      color={color}
+      disableHoverEffect={disableHoverEffect}
+      disableFocusEffect={disableFocusEffect}
+      fullWidth={fullWidth}
+    >
+      <input
+        type={type}
+        value={inputValue}
+        onChange={handleChange}
+        disabled={disabled}
+        {...rest}
+      />
+    </InputBase>
+  );
 };
 
 export default Input;
