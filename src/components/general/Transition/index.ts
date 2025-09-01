5fe77a1a62077a1b4040f21bbbf246ff7d5dd7be
hyejin <yhj970102@gmail.com>
2025-09-01 19:28:57 +0900

    ➖ Remove:  기존 Transition, Scale, Collapse, Fade, Slide 컴포넌트 삭제

--- src/components/general/Transition/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Transition, DEFAULT_DURATION } from './Transition';
-export type { TransitionProps } from './Transition';
-export { flatDuration } from './Transition.utils';
-export { TRANSITION_ARG_TYPES } from './Transition.constants';
