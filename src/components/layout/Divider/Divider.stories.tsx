0b49e7f3220e796bc762b66090eb310c241b1c57
hyejin <yhj970102@gmail.com>
2025-10-01 22:32:16 +0900

    📖 Storybook:  usage 수정/추가

--- src/components/layout/Divider/Divider.stories.tsx
+++ src/components/layout/Divider/Divider.stories.tsx
@@ -1,39 +1,51 @@
+import './custom.scss';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Divider } from '@/components/layout/Divider';
 import { Stack } from '@/components/layout/Stack';
 import { Chip } from '@/components/data-display/Chip';
+import { List, ListItem } from '@/components/data-display/List';
 
 const meta: Meta<typeof Divider> = {
   component: Divider,
   argTypes: {
     children: {
-      description: '구분선 내 콘텐츠'
+      description: '구분선 내 콘텐츠',
+      table: {
+        type: { summary: 'React.ReactNode' }
+      }
+    },
+    color: {
+      description: '구분선, content 색상',
+      table: {
+        type: { summary: 'ColorType' },
+        defaultValue: { summary: `'outline-variant'` }
+      }
     },
     contentAlign: {
       description: '구분선 내 콘텐츠 위치',
       table: {
-        type: { summary: 'left | center | right' },
-        defaultValue: { summary: 'center' }
+        type: { summary: `'start' | 'center' | 'end'` },
+        defaultValue: { summary: `'center'` }
       }
     },
     orientation: {
       description: '구분선 방향',
       table: {
-        type: { summary: 'vertical | horizontal' },
-        defaultValue: { summary: 'horizontal' }
-      }
-    },
-    variant: {
-      description: '구분선 스타일',
-      table: {
-        type: { summary: 'solid | dotted | dashed' },
-        defaultValue: { summary: 'solid' }
+        type: { summary: `'vertical' | 'horizontal'` },
+        defaultValue: { summary: `'horizontal'` }
       }
     },
     thickness: {
       description: '구분선 두께',
       type: 'number',
       defaultValue: { summary: '1' }
+    },
+    variant: {
+      description: '구분선 스타일',
+      table: {
+        type: { summary: `'solid' | 'dotted' | 'dashed'` },
+        defaultValue: { summary: `'solid'` }
+      }
     }
   }
 };
@@ -56,7 +68,7 @@ const Container = ({
       style={{
         justifyContent: 'center',
         width: isHorizontal ? '500px' : '100px',
-        height: isHorizontal ? '100px' : '500px'
+        height: isHorizontal ? '100px' : '300px'
       }}
     >
       {children}
@@ -68,6 +80,14 @@ export const BasicDivider: Story = {
   render: (args) => (
     <Container orientation={args.orientation || 'horizontal'}>
       <Divider {...args} />
+    </Container>
+  )
+};
+
+export const Thickness: Story = {
+  render: (args) => (
+    <Container orientation={args.orientation || 'horizontal'}>
+      <Divider thickness={3} {...args} />
     </Container>
   )
 };
@@ -92,57 +112,69 @@ export const Orientation: Story = {
   )
 };
 
-export const WithContents: Story = {
+export const Content: Story = {
   render: (args) => (
     <Container orientation={args.orientation || 'horizontal'}>
-      <Divider contentAlign="center" {...args}>
-        content
-      </Divider>
-      <Divider contentAlign="left" {...args}>
-        content
-      </Divider>
-      <Divider contentAlign="right" {...args}>
-        content
-      </Divider>
-      <Divider contentAlign="center" {...args}>
-        <Chip label="content" />
+      <Divider {...args}>content</Divider>
+      <Divider {...args}>
+        <Chip variant="filled">content</Chip>
       </Divider>
     </Container>
   )
 };
 
-export const Thickness: Story = {
+export const ContentAlignment: Story = {
   render: (args) => (
     <Container orientation={args.orientation || 'horizontal'}>
-      <Divider thickness={3} {...args} />
+      <Divider contentAlign="center" {...args}>
+        content
+      </Divider>
+      <Divider contentAlign="start" {...args}>
+        content
+      </Divider>
+      <Divider contentAlign="end" {...args}>
+        content
+      </Divider>
+    </Container>
+  )
+};
+
+export const CustomContent: Story = {
+  render: (args) => (
+    <Container orientation={args.orientation || 'horizontal'}>
+      <Divider className="custom-content" {...args}>
+        content
+      </Divider>
+    </Container>
+  )
+};
+
+export const Color: Story = {
+  render: (args) => (
+    <Container orientation={args.orientation || 'horizontal'}>
+      <Divider color="secondary" {...args} />
+      <Divider color="yellow-400" {...args} />
+      <Divider color="rgb(100,100,100)" variant="dashed" {...args}>
+        content
+      </Divider>
     </Container>
   )
 };
 
 export const Customization: Story = {
-  render: (args) => {
-    const isHorizontal = !args.orientation || args.orientation === 'horizontal';
-    return (
-      <Container orientation={args.orientation || 'horizontal'}>
-        <Divider
-          style={{
-            borderColor: 'error',
-            color: 'error'
-          }}
-          {...args}
-        >
-          content
-        </Divider>
-        <Divider style={{ fontWeight: '900', fontSize: '20px' }} {...args}>
-          content
-        </Divider>
-        <Divider
-          style={{ margin: isHorizontal ? '10px 50px' : '50px 10px' }}
-          {...args}
-        >
-          content
-        </Divider>
-      </Container>
-    );
-  }
+  render: (args) => (
+    <List
+      style={{
+        padding: 0,
+        border: '1px solid var(--jinni-color-outline-variant)',
+        borderRadius: '4px'
+      }}
+    >
+      <ListItem>Item 1</ListItem>
+      <Divider style={{ margin: '2px 16px' }} {...args} />
+      <ListItem>Item 2</ListItem>
+      <Divider style={{ margin: '2px 16px' }} {...args} />
+      <ListItem>Item 3</ListItem>
+    </List>
+  )
 };
