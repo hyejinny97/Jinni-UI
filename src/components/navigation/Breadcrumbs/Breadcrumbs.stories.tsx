a3f9fe500e93179547752c79e19ca75f0d83ced5
hyejin <yhj970102@gmail.com>
2025-10-21 16:35:27 +0900

    📖 Storybook:  Breadcrumbs 스토리 수정/추가

--- src/components/navigation/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/components/navigation/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { useRef, useState } from 'react';
 import Breadcrumbs from './Breadcrumbs';
 import { Stack } from '@/components/layout/Stack';
+import { Box } from '@/components/layout/Box';
 import { HomeIcon } from '@/components/icons/HomeIcon';
 import { PersonIcon } from '@/components/icons/PersonIcon';
 import { CartIcon } from '@/components/icons/CartIcon';
@@ -9,6 +10,11 @@ import { ArrowRightIcon } from '@/components/icons/ArrowRightIcon';
 import { Menu } from '@/components/navigation/Menu';
 import { MenuItem } from '@/components/navigation/MenuItem';
 import { Button } from '@/components/general/Button';
+import { ButtonBase } from '@/components/general/ButtonBase';
+import { Link } from '@/components/navigation/Link';
+import { Text } from '@/components/general/Text';
+import { Chip } from '@/components/data-display/Chip';
+import { Tree, TreeItem, TreeItemIdType } from '@/components/data-display/Tree';
 
 const meta: Meta<typeof Breadcrumbs> = {
   component: Breadcrumbs,
@@ -46,31 +52,36 @@ const ICON_LINK_STYLE = {
   columnGap: '3px'
 };
 
-const CondensedBreadcrumbsWithMenuTemplate = ({ ...args }) => {
+const WithMenu = () => {
   const anchorRef = useRef<HTMLElement>(null);
   const [open, setOpen] = useState(false);
 
-  const handleCollapsedButtonClick = () => {
+  const openMenu = () => {
     setOpen(true);
   };
-  const handleMenuClose = () => {
+  const closeMenu = () => {
     setOpen(false);
   };
 
   return (
     <>
-      <Breadcrumbs {...args}>
-        <a href="#">route1</a>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link href="#" underline="hover">
+          route1
+        </Link>
         <Button
           ref={anchorRef}
-          variant="text"
-          color="black"
+          variant="subtle-filled"
+          color="gray-600"
           size="sm"
-          onClick={handleCollapsedButtonClick}
+          onClick={openMenu}
+          aria-label="Show path"
         >
           ...
         </Button>
-        <a href="#">route4</a>
+        <Link href="#" underline="hover" aria-current="page">
+          route4
+        </Link>
       </Breadcrumbs>
       <Menu
         anchorElRef={anchorRef}
@@ -83,27 +94,103 @@ const CondensedBreadcrumbsWithMenuTemplate = ({ ...args }) => {
           vertical: 'top'
         }}
         open={open}
-        onClose={handleMenuClose}
-        onClick={handleMenuClose}
+        onClose={closeMenu}
       >
-        <MenuItem>route2</MenuItem>
-        <MenuItem>route3</MenuItem>
+        <MenuItem href="#" onClick={closeMenu}>
+          route2
+        </MenuItem>
+        <MenuItem href="#" onClick={closeMenu}>
+          route3
+        </MenuItem>
       </Menu>
     </>
+  );
+};
+
+type PageType = { [key: string]: { label: string; routes: string } };
+
+const PAGES: PageType = {
+  home: { label: 'Home', routes: '/' },
+  news: { label: 'News', routes: '/news' },
+  weather: { label: 'Weather', routes: '/news/weather' },
+  entertainment: {
+    label: 'Entertainment',
+    routes: '/news/entertainment'
+  },
+  shopping: { label: 'Shopping', routes: '/shopping' },
+  email: { label: 'Email', routes: '/email' }
+};
+
+const getRouteArr = (currentPage: string) => {
+  return ['home', ...PAGES[currentPage].routes.split('/').filter(Boolean)];
+};
+
+const BreadcrumbsWithTree = () => {
+  const [currentPage, setCurrentPage] = useState<string>('weather');
+  const routeArr = getRouteArr(currentPage);
+
+  const handleChipClick = (route: string) => {
+    setCurrentPage(route);
+  };
+  const handleSelectedItemsChange = (
+    _: Event | React.SyntheticEvent,
+    itemId: TreeItemIdType | null
+  ) => {
+    setCurrentPage(itemId as string);
+  };
+
+  return (
+    <Stack spacing={20}>
+      <Breadcrumbs itemsAfterCollapse={3} aria-label="breadcrumb">
+        {routeArr.map((route) => (
+          <Chip
+            key={route}
+            as={ButtonBase}
+            variant="subtle-filled"
+            onClick={() => handleChipClick(route)}
+          >
+            {PAGES[route].label}
+          </Chip>
+        ))}
+      </Breadcrumbs>
+      <Box style={{ width: '300px' }}>
+        <Tree
+          defaultExpandedItems={['news']}
+          selectedItems={currentPage}
+          onSelectedItemsChange={handleSelectedItemsChange}
+        >
+          <TreeItem itemId="news" label={PAGES.news.label}>
+            <TreeItem itemId="weather" label={PAGES.weather.label} />
+            <TreeItem
+              itemId="entertainment"
+              label={PAGES.entertainment.label}
+            />
+          </TreeItem>
+          <TreeItem itemId="shopping" label={PAGES.shopping.label} />
+          <TreeItem itemId="email" label={PAGES.email.label} />
+        </Tree>
+      </Box>
+    </Stack>
   );
 };
 
 export const BasicBreadcrumbs: Story = {
   render: (args) => {
     return (
-      <Stack>
-        <Breadcrumbs {...args}>
-          <a href="#">route1</a>
-          <a href="#">route2</a>
+      <Stack spacing={20}>
+        <Breadcrumbs aria-label="breadcrumb" {...args}>
+          <Link href="#" underline="hover">
+            route1
+          </Link>
+          <Link href="#" underline="hover" aria-current="page">
+            route2
+          </Link>
         </Breadcrumbs>
-        <Breadcrumbs {...args}>
-          <a href="#">route1</a>
-          <span>route2</span>
+        <Breadcrumbs aria-label="breadcrumb" {...args}>
+          <Link href="#" underline="hover">
+            route1
+          </Link>
+          <Text style={{ margin: 0, color: 'gray-700' }}>route2</Text>
         </Breadcrumbs>
       </Stack>
     );
@@ -113,19 +200,24 @@ export const BasicBreadcrumbs: Story = {
 export const BreadcrumbsWithIcons: Story = {
   render: (args) => {
     return (
-      <Breadcrumbs {...args}>
-        <a href="#" style={ICON_LINK_STYLE}>
+      <Breadcrumbs aria-label="breadcrumb" {...args}>
+        <Link href="#" underline="hover" style={ICON_LINK_STYLE}>
           <HomeIcon size={15} />
           Home
-        </a>
-        <a href="#" style={ICON_LINK_STYLE}>
+        </Link>
+        <Link href="#" underline="hover" style={ICON_LINK_STYLE}>
           <PersonIcon size={15} />
           Profile
-        </a>
-        <a href="#" style={ICON_LINK_STYLE}>
+        </Link>
+        <Link
+          href="#"
+          underline="hover"
+          style={ICON_LINK_STYLE}
+          aria-current="page"
+        >
           <CartIcon size={15} />
           Cart
-        </a>
+        </Link>
       </Breadcrumbs>
     );
   }
@@ -134,13 +226,25 @@ export const BreadcrumbsWithIcons: Story = {
 export const MaxItems: Story = {
   render: (args) => {
     return (
-      <Breadcrumbs maxItems={3} {...args}>
-        <a href="#">route1</a>
-        <a href="#">route2</a>
-        <a href="#">route3</a>
-        <a href="#">route4</a>
-        <a href="#">route5</a>
-        <a href="#">route6</a>
+      <Breadcrumbs maxItems={3} aria-label="breadcrumb" {...args}>
+        <Link href="#" underline="hover">
+          route1
+        </Link>
+        <Link href="#" underline="hover">
+          route2
+        </Link>
+        <Link href="#" underline="hover">
+          route3
+        </Link>
+        <Link href="#" underline="hover">
+          route4
+        </Link>
+        <Link href="#" underline="hover">
+          route5
+        </Link>
+        <Link href="#" underline="hover" aria-current="page">
+          route6
+        </Link>
       </Breadcrumbs>
     );
   }
@@ -149,13 +253,25 @@ export const MaxItems: Story = {
 export const ItemsBeforeCollapse: Story = {
   render: (args) => {
     return (
-      <Breadcrumbs itemsBeforeCollapse={2} {...args}>
-        <a href="#">route1</a>
-        <a href="#">route2</a>
-        <a href="#">route3</a>
-        <a href="#">route4</a>
-        <a href="#">route5</a>
-        <a href="#">route6</a>
+      <Breadcrumbs itemsBeforeCollapse={2} aria-label="breadcrumb" {...args}>
+        <Link href="#" underline="hover">
+          route1
+        </Link>
+        <Link href="#" underline="hover">
+          route2
+        </Link>
+        <Link href="#" underline="hover">
+          route3
+        </Link>
+        <Link href="#" underline="hover">
+          route4
+        </Link>
+        <Link href="#" underline="hover">
+          route5
+        </Link>
+        <Link href="#" underline="hover" aria-current="page">
+          route6
+        </Link>
       </Breadcrumbs>
     );
   }
@@ -164,37 +280,201 @@ export const ItemsBeforeCollapse: Story = {
 export const ItemsAfterCollapse: Story = {
   render: (args) => {
     return (
-      <Breadcrumbs itemsAfterCollapse={3} {...args}>
-        <a href="#">route1</a>
-        <a href="#">route2</a>
-        <a href="#">route3</a>
-        <a href="#">route4</a>
-        <a href="#">route5</a>
-        <a href="#">route6</a>
+      <Breadcrumbs itemsAfterCollapse={3} aria-label="breadcrumb" {...args}>
+        <Link href="#" underline="hover">
+          route1
+        </Link>
+        <Link href="#" underline="hover">
+          route2
+        </Link>
+        <Link href="#" underline="hover">
+          route3
+        </Link>
+        <Link href="#" underline="hover">
+          route4
+        </Link>
+        <Link href="#" underline="hover">
+          route5
+        </Link>
+        <Link href="#" underline="hover" aria-current="page">
+          route6
+        </Link>
       </Breadcrumbs>
     );
   }
 };
 
 export const CondensedBreadcrumbsWithMenu: Story = {
-  render: (args) => CondensedBreadcrumbsWithMenuTemplate(args)
+  render: () => <WithMenu />,
+  parameters: {
+    docs: {
+      source: {
+        code: `const WithMenu = () => {
+  const anchorRef = useRef<HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+
+  const openMenu = () => {
+    setOpen(true);
+  };
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link href="#" underline="hover">
+          route1
+        </Link>
+        <Button
+          ref={anchorRef}
+          variant="subtle-filled"
+          color="gray-600"
+          size="sm"
+          onClick={openMenu}
+          aria-label="Show path"
+        >
+          ...
+        </Button>
+        <Link href="#" underline="hover" aria-current="page">
+          route4
+        </Link>
+      </Breadcrumbs>
+      <Menu
+        anchorElRef={anchorRef}
+        anchorOrigin={{
+          horizontal: 'center',
+          vertical: 'bottom'
+        }}
+        menuOrigin={{
+          horizontal: 'center',
+          vertical: 'top'
+        }}
+        open={open}
+        onClose={closeMenu}
+      >
+        <MenuItem href="#" onClick={closeMenu}>
+          route2
+        </MenuItem>
+        <MenuItem href="#" onClick={closeMenu}>
+          route3
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};`.trim()
+      }
+    }
+  }
 };
 
 export const Separator: Story = {
   render: (args) => {
     return (
       <Stack spacing={20}>
-        <Breadcrumbs separator="-" {...args}>
-          <a href="#">route1</a>
-          <a href="#">route2</a>
-          <a href="#">route3</a>
+        <Breadcrumbs separator="-" aria-label="breadcrumb" {...args}>
+          <Link href="#" underline="hover">
+            route1
+          </Link>
+          <Link href="#" underline="hover">
+            route2
+          </Link>
+          <Link href="#" underline="hover" aria-current="page">
+            route3
+          </Link>
         </Breadcrumbs>
-        <Breadcrumbs separator={<ArrowRightIcon size={15} />} {...args}>
-          <a href="#">route1</a>
-          <a href="#">route2</a>
-          <a href="#">route3</a>
+        <Breadcrumbs
+          separator={<ArrowRightIcon size={15} />}
+          aria-label="breadcrumb"
+          {...args}
+        >
+          <Link href="#" underline="hover">
+            route1
+          </Link>
+          <Link href="#" underline="hover">
+            route2
+          </Link>
+          <Link href="#" underline="hover" aria-current="page">
+            route3
+          </Link>
         </Breadcrumbs>
       </Stack>
     );
+  }
+};
+
+export const Customization: Story = {
+  render: () => <BreadcrumbsWithTree />,
+  parameters: {
+    docs: {
+      source: {
+        code: `type PageType = { [key: string]: { label: string; routes: string } };
+
+const PAGES: PageType = {
+  home: { label: 'Home', routes: '/' },
+  news: { label: 'News', routes: '/news' },
+  weather: { label: 'Weather', routes: '/news/weather' },
+  entertainment: {
+    label: 'Entertainment',
+    routes: '/news/entertainment'
+  },
+  shopping: { label: 'Shopping', routes: '/shopping' },
+  email: { label: 'Email', routes: '/email' }
+};
+
+const getRouteArr = (currentPage: string) => {
+  return ['home', ...PAGES[currentPage].routes.split('/').filter(Boolean)];
+};
+
+const BreadcrumbsWithTree = () => {
+  const [currentPage, setCurrentPage] = useState<string>('weather');
+  const routeArr = getRouteArr(currentPage);
+
+  const handleChipClick = (route: string) => {
+    setCurrentPage(route);
+  };
+  const handleSelectedItemsChange = (
+    _: Event | React.SyntheticEvent,
+    itemId: TreeItemIdType | null
+  ) => {
+    setCurrentPage(itemId as string);
+  };
+
+  return (
+    <Stack spacing={20}>
+      <Breadcrumbs itemsAfterCollapse={3} aria-label="breadcrumb">
+        {routeArr.map((route) => (
+          <Chip
+            key={route}
+            as={ButtonBase}
+            variant="subtle-filled"
+            onClick={() => handleChipClick(route)}
+          >
+            {PAGES[route].label}
+          </Chip>
+        ))}
+      </Breadcrumbs>
+      <Box style={{ width: '300px' }}>
+        <Tree
+          defaultExpandedItems={['news']}
+          selectedItems={currentPage}
+          onSelectedItemsChange={handleSelectedItemsChange}
+        >
+          <TreeItem itemId="news" label={PAGES.news.label}>
+            <TreeItem itemId="weather" label={PAGES.weather.label} />
+            <TreeItem
+              itemId="entertainment"
+              label={PAGES.entertainment.label}
+            />
+          </TreeItem>
+          <TreeItem itemId="shopping" label={PAGES.shopping.label} />
+          <TreeItem itemId="email" label={PAGES.email.label} />
+        </Tree>
+      </Box>
+    </Stack>
+  );
+};`.trim()
+      }
+    }
   }
 };
