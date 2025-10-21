c101cec1d3d3e1275ea2ea3324df950f41e1cc3e
HyeJin <yhj970102@gmail.com>
2025-10-21 17:55:58 +0900

    ✨ Feat :  (v1-2차) `Breadcrumbs` 수정 (#94)
    * ✨ Feat :  `Breadcrumbs` 수정 (v1-2차)
    
    * 📖 Storybook:  Breadcrumbs 스토리 수정/추가
    
    * ♻️ Refactor :  접근성 보완
    
    * 🧪Test :  Breadcrumbs 테스트 코드 작성

--- src/components/navigation/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/navigation/Breadcrumbs/Breadcrumbs.tsx
@@ -1,11 +1,12 @@
 import './Breadcrumbs.scss';
 import cn from 'classnames';
-import { Children, Fragment } from 'react';
+import { Children, Fragment, useMemo, useState } from 'react';
 import useStyle from '@/hooks/useStyle';
 import { AsType, DefaultComponentProps } from '@/types/default-component-props';
-import useDisplay from './Breadcrumbs.hooks';
+import { Button, ButtonProps } from '@/components/general/Button';
+import { validatePositiveInteger } from '@/utils/isNumber';
 
-export type BreadcrumbsProps<T extends AsType = 'ol'> =
+export type BreadcrumbsProps<T extends AsType = 'nav'> =
   DefaultComponentProps<T> & {
     children: React.ReactNode;
     separator?: React.ReactNode;
@@ -13,6 +14,25 @@ export type BreadcrumbsProps<T extends AsType = 'ol'> =
     itemsBeforeCollapse?: number;
     itemsAfterCollapse?: number;
   };
+
+const EllipsisButton = (props: ButtonProps) => (
+  <Button
+    variant="text"
+    color="black"
+    size="sm"
+    shape="rounded"
+    aria-label="Show path"
+    {...props}
+  >
+    ...
+  </Button>
+);
+
+const BreadCrumbSeparator = ({ children }: { children: React.ReactNode }) => (
+  <li className="JinniBreadcrumbSeparator" aria-hidden="true">
+    {children}
+  </li>
+);
 
 const Breadcrumbs = <T extends AsType = 'ol'>(props: BreadcrumbsProps<T>) => {
   const {
@@ -23,22 +43,38 @@ const Breadcrumbs = <T extends AsType = 'ol'>(props: BreadcrumbsProps<T>) => {
     itemsAfterCollapse = 1,
     className,
     style,
-    as: Component = 'ol',
+    as: Component = 'nav',
     ...rest
   } = props;
-  const newStyle = useStyle(style);
-  const childrenArray = Children.toArray(children);
-  const childrenLiArray = childrenArray.map((child, childIdx) => (
-    <li className="JinniBreadcrumb" key={childIdx}>
-      {child}
-    </li>
-  ));
-  const displayedChildren = useDisplay({
-    maxItems,
-    itemsBeforeCollapse,
-    itemsAfterCollapse,
-    childrenLiArray
+  const validatedMaxItems = validatePositiveInteger({ value: maxItems });
+  const validatedItemsBeforeCollapse = validatePositiveInteger({
+    value: itemsBeforeCollapse
   });
+  const validatedItemsAfterCollapse = validatePositiveInteger({
+    value: itemsAfterCollapse
+  });
+  const items = Children.toArray(children);
+  const maxItemsToDisplay = Math.max(
+    validatedItemsBeforeCollapse + validatedItemsAfterCollapse,
+    validatedMaxItems
+  );
+  const [collapsed, setCollapsed] = useState(items.length > maxItemsToDisplay);
+  const newStyle = useStyle(style);
+
+  const itemsToDisplay = useMemo(() => {
+    return collapsed
+      ? [
+          ...items.slice(0, validatedItemsBeforeCollapse),
+          <EllipsisButton onClick={() => setCollapsed((prev) => !prev)} />,
+          ...items.slice(items.length - validatedItemsAfterCollapse)
+        ]
+      : items;
+  }, [
+    items,
+    validatedItemsBeforeCollapse,
+    validatedItemsAfterCollapse,
+    collapsed
+  ]);
 
   return (
     <Component
@@ -46,19 +82,19 @@ const Breadcrumbs = <T extends AsType = 'ol'>(props: BreadcrumbsProps<T>) => {
       style={newStyle}
       {...rest}
     >
-      {displayedChildren.map((child, childIdx) => {
-        const isLastChild = childIdx === displayedChildren.length - 1;
-        return (
-          <Fragment key={childIdx}>
-            {child}
-            {!isLastChild && (
-              <li className="JinniSeparator" aria-hidden="true">
-                {separator}
-              </li>
-            )}
-          </Fragment>
-        );
-      })}
+      <ol>
+        {itemsToDisplay.map((item, idx) => {
+          const isLastChild = idx === itemsToDisplay.length - 1;
+          return (
+            <Fragment key={idx}>
+              <li>{item}</li>
+              {!isLastChild && (
+                <BreadCrumbSeparator>{separator}</BreadCrumbSeparator>
+              )}
+            </Fragment>
+          );
+        })}
+      </ol>
     </Component>
   );
 };
