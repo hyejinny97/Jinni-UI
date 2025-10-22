7de16edfef292264dbbdbb8cbc29d3a5adbaeb5b
HyeJin <yhj970102@gmail.com>
2025-10-22 18:26:35 +0900

    ✨ Feat :  (v1-2차) `Text` 수정 (#95)
    * ✨ Feat :  `Text` 수정 (v1-2차)
    
    * 📖 Storybook:  스토리 추가/수정
    
    * 🧪Test :  Text 테스트 코드 작성

--- src/components/general/Text/Text.tsx
+++ src/components/general/Text/Text.tsx
@@ -1,33 +1,47 @@
 import './Text.scss';
 import cn from 'classnames';
+import { forwardRef } from 'react';
 import { AsType, DefaultComponentProps } from '@/types/default-component-props';
 import useStyle from '@/hooks/useStyle';
+import { validatePositiveInteger } from '@/utils/isNumber';
 
 type TextProps<T extends AsType = 'p'> = DefaultComponentProps<T> & {
   children: React.ReactNode;
   lineClamp?: number;
+  noMargin?: boolean;
 };
 
-const Text = <T extends AsType = 'p'>(props: TextProps<T>) => {
-  const {
-    children,
-    lineClamp,
-    className,
-    style,
-    as: Component = 'p',
-    ...rest
-  } = props;
-  const newStyle = useStyle({ '--line-clamp': lineClamp, ...style });
+const Text = forwardRef(
+  <T extends AsType = 'p'>(
+    props: TextProps<T>,
+    ref: React.Ref<HTMLElement>
+  ) => {
+    const {
+      children,
+      lineClamp,
+      noMargin,
+      className,
+      style,
+      as: Component = 'p',
+      ...rest
+    } = props;
+    const newStyle = useStyle({
+      '--line-clamp':
+        lineClamp && validatePositiveInteger({ value: lineClamp }),
+      ...style
+    });
 
-  return (
-    <Component
-      className={cn('JinniText', { lineClamp }, className)}
-      style={newStyle}
-      {...rest}
-    >
-      {children}
-    </Component>
-  );
-};
+    return (
+      <Component
+        ref={ref}
+        className={cn('JinniText', { lineClamp, noMargin }, className)}
+        style={newStyle}
+        {...rest}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
 
 export default Text;
