4de7306108d9116223ccefd49920a493504cf765
hyejin <yhj970102@gmail.com>
2025-10-31 01:24:46 +0900

    📖 Storybook:  스토리 수정

--- src/components/layout/Grid/Grid.stories.tsx
+++ src/components/layout/Grid/Grid.stories.tsx
@@ -1,9 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 import Grid from './Grid';
-import GridItem from './GridItem';
 import { Box } from '@/components/layout/Box';
+import { Text } from '@/components/general/Text';
 import { Stack } from '@/components/layout/Stack';
-import { Divider } from '@/components/layout/Divider';
+import { StyleType } from '@/types/style';
+import { Radio } from '@/components/data-entry/Radio';
+import { RadioLabel } from '@/components/data-entry/RadioLabel';
 
 const meta: Meta<typeof Grid> = {
   component: Grid,
@@ -12,7 +15,7 @@ const meta: Meta<typeof Grid> = {
       description: 'grid items'
     },
     columns: {
-      description: 'grid container 내 동일한 크기의 column 갯수',
+      description: '동일한 크기의 column 갯수',
       table: {
         type: { summary: `number | 'auto' | Responsive<number | 'auto'>` },
         defaultValue: { summary: `'auto'` }
@@ -34,7 +37,7 @@ const meta: Meta<typeof Grid> = {
       }
     },
     rows: {
-      description: 'grid container 내 동일한 크기의 row 갯수',
+      description: '동일한 크기의 row 갯수',
       table: {
         type: { summary: `number | 'auto' | Responsive<number | 'auto'>` },
         defaultValue: { summary: `'auto'` }
@@ -58,27 +61,135 @@ const meta: Meta<typeof Grid> = {
 export default meta;
 type Story = StoryObj<typeof Grid>;
 
-const Container = ({ children }: { children: React.ReactNode }) => (
-  <Stack spacing={20}>{children}</Stack>
-);
-
-const Item = ({ children }: { children: React.ReactNode }) => (
+const GrayBox = ({
+  children,
+  style
+}: {
+  children: React.ReactNode;
+  style?: StyleType;
+}) => (
   <Box
     round="sm"
     style={{
       boxSizing: 'border-box',
-      display: 'flex',
+      display: 'inline-flex',
       alignItems: 'center',
       justifyContent: 'center',
       width: '100%',
       height: '100%',
       padding: '10px',
-      backgroundColor: 'gray-200'
+      backgroundColor: 'gray-200',
+      ...style
     }}
   >
     {children}
   </Box>
 );
+
+const GridFlowTemplate = () => {
+  const FLOWS = [
+    'row',
+    'column',
+    'dense',
+    'row-dense',
+    'column-dense'
+  ] as const;
+  const [checkedValue, setCheckedValue] = useState<(typeof FLOWS)[number]>(
+    FLOWS[0]
+  );
+
+  const check = (value: (typeof FLOWS)[number]) => () => {
+    setCheckedValue(value);
+  };
+
+  return (
+    <Stack spacing={30}>
+      <Stack direction="row" spacing={10}>
+        {FLOWS.map((flow) => (
+          <RadioLabel label={flow}>
+            <Radio checked={checkedValue === flow} onChange={check(flow)} />
+          </RadioLabel>
+        ))}
+      </Stack>
+      <Grid rows={3} columns={3} spacing={10} flow={checkedValue}>
+        <GrayBox style={{ gridColumn: 'span 2' }}>Grid Item 1</GrayBox>
+        <GrayBox style={{ gridColumn: 'span 2' }}>Grid Item 2</GrayBox>
+        <GrayBox>Grid Item 3</GrayBox>
+        <GrayBox>Grid Item 4</GrayBox>
+        <GrayBox>Grid Item 5</GrayBox>
+      </Grid>
+    </Stack>
+  );
+};
+
+const AlignmentTemplate = () => {
+  const POSITIONS = ['start', 'center', 'end'] as const;
+  const [justifyItems, setJustifyItems] = useState<(typeof POSITIONS)[number]>(
+    POSITIONS[1]
+  );
+  const [alignItems, setAlignItems] = useState<(typeof POSITIONS)[number]>(
+    POSITIONS[1]
+  );
+
+  const changeJustifyItems = (value: (typeof POSITIONS)[number]) => () => {
+    setJustifyItems(value);
+  };
+  const changeAlignItems = (value: (typeof POSITIONS)[number]) => () => {
+    setAlignItems(value);
+  };
+
+  return (
+    <Stack spacing={30}>
+      <Stack>
+        <Stack direction="row" spacing={10}>
+          <Text>justify-items: </Text>
+          {POSITIONS.map((position) => (
+            <RadioLabel label={position}>
+              <Radio
+                checked={justifyItems === position}
+                onChange={changeJustifyItems(position)}
+              />
+            </RadioLabel>
+          ))}
+        </Stack>
+        <Stack direction="row" spacing={10}>
+          <Text>align-items: </Text>
+          {POSITIONS.map((position) => (
+            <RadioLabel label={position}>
+              <Radio
+                checked={alignItems === position}
+                onChange={changeAlignItems(position)}
+              />
+            </RadioLabel>
+          ))}
+        </Stack>
+      </Stack>
+      <Grid
+        columns={3}
+        spacing={10}
+        style={{
+          justifyItems,
+          alignItems,
+          width: '500px',
+          height: '100px',
+          border: '1px solid lightgray',
+          borderRadius: '4px'
+        }}
+      >
+        {Array(3)
+          .fill(0)
+          .map((_, idx) => (
+            <GrayBox
+              key={idx}
+              style={{ width: 'max-content', height: 'max-content' }}
+            >
+              Grid Item {idx + 1}
+            </GrayBox>
+          ))}
+      </Grid>
+    </Stack>
+  );
+};
 
 export const BasicGrid: Story = {
   render: (args) => (
@@ -86,9 +197,7 @@ export const BasicGrid: Story = {
       {Array(6)
         .fill(0)
         .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
+          <GrayBox key={idx}>Grid Item {idx + 1}</GrayBox>
         ))}
     </Grid>
   )
@@ -105,9 +214,7 @@ export const ResponsiveGrid: Story = {
       {Array(6)
         .fill(0)
         .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
+          <GrayBox key={idx}>Grid Item {idx + 1}</GrayBox>
         ))}
     </Grid>
   )
@@ -119,9 +226,7 @@ export const Spacing: Story = {
       {Array(6)
         .fill(0)
         .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
+          <GrayBox key={idx}>Grid Item {idx + 1}</GrayBox>
         ))}
     </Grid>
   )
@@ -133,9 +238,7 @@ export const RowColumnSpacing: Story = {
       {Array(6)
         .fill(0)
         .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
+          <GrayBox key={idx}>Grid Item {idx + 1}</GrayBox>
         ))}
     </Grid>
   )
@@ -143,13 +246,11 @@ export const RowColumnSpacing: Story = {
 
 export const ResponsiveSpacing: Story = {
   render: (args) => (
-    <Grid rows={2} columns={3} spacing={{ xs: 10, md: '3rem' }} {...args}>
+    <Grid rows={2} columns={3} spacing={{ xs: '10px', md: '3rem' }} {...args}>
       {Array(6)
         .fill(0)
         .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
+          <GrayBox key={idx}>Grid Item {idx + 1}</GrayBox>
         ))}
     </Grid>
   )
@@ -158,15 +259,15 @@ export const ResponsiveSpacing: Story = {
 export const Span: Story = {
   render: (args) => (
     <Grid rows={3} columns={4} spacing={10} {...args}>
-      <GridItem rowSpan="full" columnSpan={2}>
-        <Item>Grid Item 1</Item>
-      </GridItem>
-      <GridItem columnSpan={2}>
-        <Item>Grid Item 2</Item>
-      </GridItem>
-      <GridItem rowSpan={2} columnSpan={2}>
-        <Item>Grid Item 3</Item>
-      </GridItem>
+      <GrayBox
+        style={{ gridRowStart: 1, gridRowEnd: -1, gridColumn: 'span 2' }}
+      >
+        Grid Item 1
+      </GrayBox>
+      <GrayBox style={{ gridColumn: 'span 2' }}>Grid Item 2</GrayBox>
+      <GrayBox style={{ gridRow: 'span 2', gridColumn: 'span 2' }}>
+        Grid Item 3
+      </GrayBox>
     </Grid>
   )
 };
@@ -174,15 +275,15 @@ export const Span: Story = {
 export const RowStartEnd: Story = {
   render: (args) => (
     <Grid rows={3} spacing={10} style={{ height: '200px' }} {...args}>
-      <GridItem rowStart={2} rowSpan={2}>
-        <Item>Grid Item 1</Item>
-      </GridItem>
-      <GridItem rowEnd={3} rowSpan={2}>
-        <Item>Grid Item 2</Item>
-      </GridItem>
-      <GridItem rowStart={1} rowEnd={4}>
-        <Item>Grid Item 3</Item>
-      </GridItem>
+      <GrayBox style={{ gridRowStart: 2, gridRowEnd: 'span 2' }}>
+        Grid Item 1
+      </GrayBox>
+      <GrayBox style={{ gridRowStart: 'span 2', gridRowEnd: 3 }}>
+        Grid Item 2
+      </GrayBox>
+      <GrayBox style={{ gridRow: { xs: '1 / 2', md: '1 / 4' } }}>
+        Grid Item 3
+      </GrayBox>
     </Grid>
   )
 };
@@ -190,122 +291,84 @@ export const RowStartEnd: Story = {
 export const ColumnStartEnd: Story = {
   render: (args) => (
     <Grid columns={6} spacing={10} {...args}>
-      <GridItem columnStart={2} columnSpan={4}>
-        <Item>Grid Item 1</Item>
-      </GridItem>
-      <GridItem columnStart={1} columnEnd={3}>
-        <Item>Grid Item 2</Item>
-      </GridItem>
-      <GridItem columnEnd={7} columnSpan={2}>
-        <Item>Grid Item 3</Item>
-      </GridItem>
+      <GrayBox style={{ gridColumnStart: 2, gridColumnEnd: 'span 4' }}>
+        Grid Item 1
+      </GrayBox>
+      <GrayBox style={{ gridColumn: '1 / 3' }}>Grid Item 2</GrayBox>
+      <GrayBox
+        style={{ gridColumnStart: 'span 2', gridColumnEnd: { xs: 6, md: 7 } }}
+      >
+        Grid Item 3
+      </GrayBox>
     </Grid>
   )
 };
 
 export const GridFlow: Story = {
-  render: (args) => (
-    <Container>
-      <Divider>row</Divider>
-      <Grid rows={3} columns={3} spacing={10} flow="row" {...args}>
-        <GridItem columnSpan={2}>
-          <Item>Grid Item 1</Item>
-        </GridItem>
-        <GridItem columnSpan={2}>
-          <Item>Grid Item 2</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 3</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 4</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 5</Item>
-        </GridItem>
+  render: () => <GridFlowTemplate />,
+  parameters: {
+    docs: {
+      source: {
+        code: `const GridFlowTemplate = () => {
+  const FLOWS = [
+    'row',
+    'column',
+    'dense',
+    'row-dense',
+    'column-dense'
+  ] as const;
+  const [checkedValue, setCheckedValue] = useState<(typeof FLOWS)[number]>(
+    FLOWS[0]
+  );
+
+  const check = (value: (typeof FLOWS)[number]) => () => {
+    setCheckedValue(value);
+  };
+
+  return (
+    <Stack spacing={30}>
+      <Stack direction="row" spacing={10}>
+        {FLOWS.map((flow) => (
+          <RadioLabel label={flow}>
+            <Radio checked={checkedValue === flow} onChange={check(flow)} />
+          </RadioLabel>
+        ))}
+      </Stack>
+      <Grid rows={3} columns={3} spacing={10} flow={checkedValue}>
+        <GrayBox style={{ gridColumn: 'span 2' }}>Grid Item 1</GrayBox>
+        <GrayBox style={{ gridColumn: 'span 2' }}>Grid Item 2</GrayBox>
+        <GrayBox>Grid Item 3</GrayBox>
+        <GrayBox>Grid Item 4</GrayBox>
+        <GrayBox>Grid Item 5</GrayBox>
       </Grid>
-      <Divider>row-dense</Divider>
-      <Grid rows={3} columns={3} spacing={10} flow="row-dense" {...args}>
-        <GridItem columnSpan={2}>
-          <Item>Grid Item 1</Item>
-        </GridItem>
-        <GridItem columnSpan={2}>
-          <Item>Grid Item 2</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 3</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 4</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 5</Item>
-        </GridItem>
-      </Grid>
-      <Divider>column</Divider>
-      <Grid rows={3} columns={3} spacing={10} flow="column" {...args}>
-        <GridItem columnSpan={2}>
-          <Item>Grid Item 1</Item>
-        </GridItem>
-        <GridItem columnSpan={2}>
-          <Item>Grid Item 2</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 3</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 4</Item>
-        </GridItem>
-        <GridItem>
-          <Item>Grid Item 5</Item>
-        </GridItem>
-      </Grid>
-    </Container>
-  )
+    </Stack>
+  );
+};`.trim()
+      }
+    }
+  }
 };
 
 export const NestedGrid: Story = {
   render: (args) => (
     <Grid rows={4} columns={3} spacing={10} flow="column" {...args}>
-      <GridItem>
-        <Item>Grid Item 1</Item>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 2</Item>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 3</Item>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 4</Item>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 5</Item>
-      </GridItem>
-      <GridItem rowSpan={3}>
-        <Grid
-          rows={3}
-          spacing={10}
-          flow="column"
-          style={{ backgroundColor: 'yellow-50' }}
-        >
-          <GridItem rowStart={2}>
-            <Item>Grid Item 6</Item>
-          </GridItem>
-        </Grid>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 7</Item>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 8</Item>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 9</Item>
-      </GridItem>
-      <GridItem>
-        <Item>Grid Item 10</Item>
-      </GridItem>
+      <GrayBox>Grid Item 1</GrayBox>
+      <GrayBox>Grid Item 2</GrayBox>
+      <GrayBox>Grid Item 3</GrayBox>
+      <GrayBox>Grid Item 4</GrayBox>
+      <GrayBox>Grid Item 5</GrayBox>
+      <Grid
+        rows={3}
+        spacing={10}
+        flow="column"
+        style={{ gridRow: 'span 3', backgroundColor: 'yellow-50' }}
+      >
+        <GrayBox style={{ gridRowStart: 2 }}>Grid Item 6</GrayBox>
+      </Grid>
+      <GrayBox>Grid Item 7</GrayBox>
+      <GrayBox>Grid Item 8</GrayBox>
+      <GrayBox>Grid Item 9</GrayBox>
+      <GrayBox>Grid Item 10</GrayBox>
     </Grid>
   )
 };
@@ -323,9 +386,7 @@ export const GridTemplateCustomization: Story = {
       {Array(6)
         .fill(0)
         .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
+          <GrayBox key={idx}>Grid Item {idx + 1}</GrayBox>
         ))}
     </Grid>
   )
@@ -336,17 +397,15 @@ export const GridAuto: Story = {
     <Grid
       spacing={10}
       style={{
-        gridAutoRows: 'minmax(60px, auto)',
-        gridAutoColumns: 'max-content'
+        gridAutoRows: 'minmax(100px, auto)',
+        gridAutoColumns: 'minmax(200px, auto)'
       }}
       {...args}
     >
       {Array(3)
         .fill(0)
         .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
+          <GrayBox key={idx}>Grid Item {idx + 1}</GrayBox>
         ))}
     </Grid>
   )
@@ -358,44 +417,118 @@ export const GridTemplateArea: Story = {
       spacing={10}
       style={{
         gridTemplateAreas: `"header header"
-                          "nav main"
-                          "nav footer"`,
+                        "main aside"
+                        "footer footer"`,
         gridTemplateRows: '50px 1fr 30px',
-        gridTemplateColumns: '150px 1fr'
+        gridTemplateColumns: '150px 1fr',
+        height: '200px'
       }}
       {...args}
     >
-      <GridItem style={{ gridArea: 'header' }}>
-        <Item>Header</Item>
-      </GridItem>
-      <GridItem style={{ gridArea: 'nav' }}>
-        <Item>Nav</Item>
-      </GridItem>
-      <GridItem style={{ gridArea: 'main' }}>
-        <Item>Main</Item>
-      </GridItem>
-      <GridItem style={{ gridArea: 'footer' }}>
-        <Item>Footer</Item>
-      </GridItem>
+      <GrayBox style={{ gridArea: 'header' }}>Header</GrayBox>
+      <GrayBox style={{ gridArea: 'main' }}>Main</GrayBox>
+      <GrayBox style={{ gridArea: 'aside' }}>Aside</GrayBox>
+      <GrayBox style={{ gridArea: 'footer' }}>Footer</GrayBox>
     </Grid>
-  )
+  ),
+  parameters: {
+    docs: {
+      source: {
+        code: `<Grid
+  spacing={10}
+  style={{
+    gridTemplateAreas: \`"header header"
+                        "main aside"
+                        "footer footer"\`,
+    gridTemplateRows: '50px 1fr 30px',
+    gridTemplateColumns: '150px 1fr',
+    height: '200px'
+  }}
+>
+  <GrayBox style={{ gridArea: 'header' }}>Header</GrayBox>
+  <GrayBox style={{ gridArea: 'main' }}>Main</GrayBox>
+  <GrayBox style={{ gridArea: 'aside' }}>Aside</GrayBox>
+  <GrayBox style={{ gridArea: 'footer' }}>Footer</GrayBox>
+</Grid>`.trim()
+      }
+    }
+  }
 };
 
 export const Alignment: Story = {
-  render: (args) => (
-    <Grid
-      columns={3}
-      spacing={10}
-      style={{ justifyItems: 'center', alignItems: 'center' }}
-      {...args}
-    >
-      {Array(3)
-        .fill(0)
-        .map((_, idx) => (
-          <GridItem key={idx}>
-            <Item>Grid Item {idx + 1}</Item>
-          </GridItem>
-        ))}
-    </Grid>
-  )
+  render: () => <AlignmentTemplate />,
+  parameters: {
+    docs: {
+      source: {
+        code: `const AlignmentTemplate = () => {
+  const POSITIONS = ['start', 'center', 'end'] as const;
+  const [justifyItems, setJustifyItems] = useState<(typeof POSITIONS)[number]>(
+    POSITIONS[1]
+  );
+  const [alignItems, setAlignItems] = useState<(typeof POSITIONS)[number]>(
+    POSITIONS[1]
+  );
+
+  const changeJustifyItems = (value: (typeof POSITIONS)[number]) => () => {
+    setJustifyItems(value);
+  };
+  const changeAlignItems = (value: (typeof POSITIONS)[number]) => () => {
+    setAlignItems(value);
+  };
+
+  return (
+    <Stack spacing={30}>
+      <Stack>
+        <Stack direction="row" spacing={10}>
+          <Text>justify-items: </Text>
+          {POSITIONS.map((position) => (
+            <RadioLabel label={position}>
+              <Radio
+                checked={justifyItems === position}
+                onChange={changeJustifyItems(position)}
+              />
+            </RadioLabel>
+          ))}
+        </Stack>
+        <Stack direction="row" spacing={10}>
+          <Text>align-items: </Text>
+          {POSITIONS.map((position) => (
+            <RadioLabel label={position}>
+              <Radio
+                checked={alignItems === position}
+                onChange={changeAlignItems(position)}
+              />
+            </RadioLabel>
+          ))}
+        </Stack>
+      </Stack>
+      <Grid
+        columns={3}
+        spacing={10}
+        style={{
+          justifyItems,
+          alignItems,
+          width: '500px',
+          height: '100px',
+          border: '1px solid lightgray',
+          borderRadius: '4px'
+        }}
+      >
+        {Array(3)
+          .fill(0)
+          .map((_, idx) => (
+            <GrayBox
+              key={idx}
+              style={{ width: 'max-content', height: 'max-content' }}
+            >
+              Grid Item {idx + 1}
+            </GrayBox>
+          ))}
+      </Grid>
+    </Stack>
+  );
+};`.trim()
+      }
+    }
+  }
 };
