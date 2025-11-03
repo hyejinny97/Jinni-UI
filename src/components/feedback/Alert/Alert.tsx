d61b50f748b9f3d831c99558ae897f8ea68a5628
hyejin <yhj970102@gmail.com>
2025-11-03 16:28:46 +0900

    ✨ Feat :  `Alert` 수정 (v1-2차)

--- src/components/feedback/Alert/Alert.tsx
+++ src/components/feedback/Alert/Alert.tsx
@@ -2,23 +2,21 @@ import './Alert.scss';
 import cn from 'classnames';
 import useStyle from '@/hooks/useStyle';
 import { AsType, DefaultComponentProps } from '@/types/default-component-props';
-import { getDefaultIconBySeverity } from './Alert.utills';
+import { getDefaultIconByStatus } from './Alert.utils';
 
-export type SeverityType = 'success' | 'info' | 'warning' | 'error';
-
-type AlertProps<T extends AsType = 'div'> = DefaultComponentProps<T> & {
+export type AlertProps<T extends AsType = 'div'> = DefaultComponentProps<T> & {
   children: React.ReactNode;
-  severity?: SeverityType;
+  title?: React.ReactNode;
+  action?: React.ReactNode;
+  status?: 'success' | 'info' | 'warning' | 'error';
   variant?: 'subtle-filled' | 'filled' | 'outlined';
   icon?: React.ReactNode | false;
-  action?: React.ReactNode;
-  title?: React.ReactNode;
 };
 
 const Alert = <T extends AsType = 'div'>(props: AlertProps<T>) => {
   const {
     children,
-    severity = 'success',
+    status = 'success',
     variant = 'subtle-filled',
     icon,
     action,
@@ -30,23 +28,18 @@ const Alert = <T extends AsType = 'div'>(props: AlertProps<T>) => {
   } = props;
   const hasTitle = !!title;
   const hasIcon = icon !== false;
-  const isDefaultIcon = !icon && hasIcon;
-  const computedIcon = isDefaultIcon
-    ? getDefaultIconBySeverity({ severity })
-    : icon;
+  const statusIcon =
+    icon === undefined ? getDefaultIconByStatus({ status }) : icon;
   const newStyle = useStyle(style);
 
   return (
     <Component
-      className={cn('JinniAlert', severity, variant, className)}
+      role="alert"
+      className={cn('JinniAlert', { hasTitle }, status, variant, className)}
       style={newStyle}
       {...rest}
     >
-      {hasIcon && (
-        <div className={cn('JinniAlertIcon', { isDefaultIcon })}>
-          {computedIcon}
-        </div>
-      )}
+      {hasIcon && <div className={cn('JinniAlertIcon')}>{statusIcon}</div>}
       <div className="JinniAlertMessage">
         {hasTitle && <div className="JinniAlertMessageTitle">{title}</div>}
         {children}
