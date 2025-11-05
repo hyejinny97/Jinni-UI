c9e356025c44db9c067d35943bfd0013880d85cb
hyejin <yhj970102@gmail.com>
2025-11-05 22:33:49 +0900

    ✨ Feat :  TransitionComponent prop 추가

--- src/components/feedback/Toast/Toast.tsx
+++ src/components/feedback/Toast/Toast.tsx
@@ -1,10 +1,13 @@
 import './Toast.scss';
 import cn from 'classnames';
+import { forwardRef } from 'react';
 import { createPortal } from 'react-dom';
 import useStyle from '@/hooks/useStyle';
 import { AsType, DefaultComponentProps } from '@/types/default-component-props';
 import { useClose } from './Toast.hooks';
 import { Box, BoxProps } from '@/components/layout/Box';
+import { Motion } from '@/components/motion/Motion';
+import { AnimatePresence } from '@/components/motion/AnimatePresence';
 
 export type CloseReason = 'timeout' | 'backgroundClick' | 'escapeKeydown';
 export type AnchorOriginType = {
@@ -24,6 +27,7 @@ export type ToastProps<T extends AsType = 'div'> = DefaultComponentProps<T> & {
   autoHideDuration?: number | null;
   anchorOrigin?: AnchorOriginType;
   BoxProps?: BoxProps;
+  TransitionComponent?: React.ComponentType<{ children: React.ReactNode }>;
 };
 
 const DEFAULT_ANCHOR_ORIGIN: AnchorOriginType = {
@@ -31,60 +35,85 @@ const DEFAULT_ANCHOR_ORIGIN: AnchorOriginType = {
   vertical: 'bottom'
 };
 
-const Toast = <T extends AsType = 'div'>(props: ToastProps<T>) => {
-  const {
-    open,
-    onClose,
-    message,
-    action,
-    autoHideDuration = null,
-    anchorOrigin = DEFAULT_ANCHOR_ORIGIN,
-    children,
-    BoxProps,
-    className,
-    style,
-    as: Component = 'div',
-    ...rest
-  } = props;
-  const { pauseTimer, resumeTimer } = useClose({
-    onClose,
-    open,
-    autoHideDuration
-  });
-  const newStyle = useStyle(style);
-
+const ScaleFade = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      {open &&
-        createPortal(
-          <Component
-            className={cn(
-              'JinniToast',
-              anchorOrigin.horizontal,
-              anchorOrigin.vertical,
-              className
-            )}
-            style={newStyle}
-            onMouseOver={pauseTimer}
-            onMouseLeave={resumeTimer}
-            {...rest}
-          >
-            {children || (
-              <Box
-                className={cn('JinniToastContent', className)}
-                elevation={3}
-                round={4}
-                {...BoxProps}
-              >
-                <div className="JinniToastContentMessage">{message}</div>
-                <div className="JinniToastContentAction">{action}</div>
-              </Box>
-            )}
-          </Component>,
-          document.body
-        )}
-    </>
+    <Motion
+      initial={{ transform: 'scale(0.9)', opacity: 0 }}
+      animate={{ transform: 'scale(1)', opacity: 1 }}
+      exit={{ transform: 'scale(0.9)', opacity: 0 }}
+      transition="transform var(--jinni-duration-short3) var(--jinni-easing-emphasized), opacity var(--jinni-duration-short3) var(--jinni-easing-emphasized)"
+    >
+      {children}
+    </Motion>
   );
 };
+
+const Toast = forwardRef(
+  <T extends AsType = 'div'>(
+    props: ToastProps<T>,
+    ref: React.Ref<HTMLElement>
+  ) => {
+    const {
+      open,
+      onClose,
+      message,
+      action,
+      autoHideDuration = null,
+      anchorOrigin = DEFAULT_ANCHOR_ORIGIN,
+      children,
+      BoxProps,
+      TransitionComponent = ScaleFade,
+      className,
+      style,
+      as: Component = 'div',
+      ...rest
+    } = props;
+    const { pauseTimer, resumeTimer } = useClose({
+      onClose,
+      open,
+      autoHideDuration
+    });
+    const newStyle = useStyle(style);
+
+    return (
+      <AnimatePresence>
+        {open && (
+          <>
+            {createPortal(
+              <Component
+                ref={ref}
+                className={cn(
+                  'JinniToast',
+                  anchorOrigin.horizontal,
+                  anchorOrigin.vertical,
+                  className
+                )}
+                style={newStyle}
+                onMouseOver={pauseTimer}
+                onMouseLeave={resumeTimer}
+                {...rest}
+              >
+                <TransitionComponent>
+                  {children || (
+                    <Box
+                      className={cn('JinniToastContent', className)}
+                      elevation={3}
+                      round={4}
+                      {...BoxProps}
+                    >
+                      <div className="JinniToastContentMessage">{message}</div>
+                      <div className="JinniToastContentAction">{action}</div>
+                    </Box>
+                  )}
+                </TransitionComponent>
+              </Component>,
+              document.body
+            )}
+          </>
+        )}
+      </AnimatePresence>
+    );
+  }
+);
 
 export default Toast;
