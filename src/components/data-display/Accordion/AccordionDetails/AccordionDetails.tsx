1dd487c24a6c514f11aab4f6bbb62979cfe8cd7d
hyejin <yhj970102@gmail.com>
2025-11-13 16:24:53 +0900

    ✨ Feat :  TransitionComponent prop 추가

--- src/components/data-display/Accordion/AccordionDetails/AccordionDetails.tsx
+++ src/components/data-display/Accordion/AccordionDetails/AccordionDetails.tsx
@@ -1,32 +1,70 @@
 import cn from 'classnames';
+import { useLayoutEffect, useRef, useState } from 'react';
 import { AsType, DefaultComponentProps } from '@/types/default-component-props';
 import useStyle from '@/hooks/useStyle';
 import { useAccordionItem } from '../AccordionItem';
+import { Motion } from '@/components/motion/Motion';
+import { AnimatePresence } from '@/components/motion/AnimatePresence';
 
 export type AccordionDetailsProps<T extends AsType = 'div'> =
   DefaultComponentProps<T> & {
     children: React.ReactNode;
+    TransitionComponent?: React.ComponentType<{ children: React.ReactNode }>;
   };
+
+const Collapse = ({ children }: { children: React.ReactNode }) => {
+  const motionElRef = useRef<HTMLElement>(null);
+  const [height, setHeight] = useState<string>('auto');
+
+  useLayoutEffect(() => {
+    const motionEl = motionElRef.current;
+    if (!motionEl) return;
+
+    setHeight(`${motionEl.scrollHeight}px`);
+  }, [children]);
+
+  return (
+    <Motion
+      ref={motionElRef}
+      initial={{ height: '0' }}
+      animate={{ height }}
+      exit={{ height: '0' }}
+      transition="height var(--jinni-duration-short3) var(--jinni-easing-emphasized)"
+      style={{ overflow: 'hidden' }}
+    >
+      {children}
+    </Motion>
+  );
+};
 
 const AccordionDetails = <T extends AsType = 'div'>(
   props: AccordionDetailsProps<T>
 ) => {
-  const { children, className, style, as: Component = 'div', ...rest } = props;
+  const {
+    children,
+    className,
+    style,
+    as: Component = 'div',
+    TransitionComponent = Collapse,
+    ...rest
+  } = props;
   const { isExpanded, disabled } = useAccordionItem();
   const newStyle = useStyle(style);
 
   return (
-    <>
+    <AnimatePresence>
       {isExpanded && !disabled && (
-        <Component
-          className={cn('JinniAccordionDetails', className)}
-          style={newStyle}
-          {...rest}
-        >
-          {children}
-        </Component>
+        <TransitionComponent>
+          <Component
+            className={cn('JinniAccordionDetails', className)}
+            style={newStyle}
+            {...rest}
+          >
+            {children}
+          </Component>
+        </TransitionComponent>
       )}
-    </>
+    </AnimatePresence>
   );
 };
 
