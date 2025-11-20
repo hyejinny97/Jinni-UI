2a2fb3623ab0db83666e09347d2440f974e1280f
hyejin <yhj970102@gmail.com>
2025-11-20 16:52:29 +0900

    ✨ Feat : `SwipeableDrawer` 구현 (미완)

--- src/components/feedback/Backdrop/Backdrop.tsx
+++ src/components/feedback/Backdrop/Backdrop.tsx
@@ -1,5 +1,6 @@
 import './Backdrop.scss';
 import cn from 'classnames';
+import React, { forwardRef, MutableRefObject } from 'react';
 import { createPortal } from 'react-dom';
 import useStyle from '@/hooks/useStyle';
 import { AsType, DefaultComponentProps } from '@/types/default-component-props';
@@ -13,47 +14,63 @@ type BackdropProps<T extends AsType = 'div'> = DefaultComponentProps<T> & {
   disablePortal?: boolean;
 };
 
-const Backdrop = <T extends AsType = 'div'>(props: BackdropProps<T>) => {
-  const {
-    children,
-    onClick,
-    invisible,
-    disableScroll,
-    disablePortal,
-    className,
-    style,
-    as: Component = 'div',
-    ...rest
-  } = props;
-  const { theme } = useJinni();
-  const backdropElRef = useWindowScroll({ disableScroll });
-  const newStyle = useStyle(style);
+const Backdrop = forwardRef(
+  <T extends AsType = 'div'>(
+    props: BackdropProps<T>,
+    ref: React.Ref<HTMLElement>
+  ) => {
+    const {
+      children,
+      onClick,
+      invisible,
+      disableScroll,
+      disablePortal,
+      className,
+      style,
+      as: Component = 'div',
+      ...rest
+    } = props;
+    const { theme } = useJinni();
+    const backdropElRef = useWindowScroll({ disableScroll });
+    const newStyle = useStyle(style);
 
-  const handleBackdropClick = (e: React.MouseEvent) => {
-    const { target, currentTarget } = e;
-    if (target !== currentTarget || !onClick) return;
-    onClick(e);
-  };
+    const handleBackdropClick = (e: React.MouseEvent) => {
+      const { target, currentTarget } = e;
+      if (target !== currentTarget || !onClick) return;
+      onClick(e);
+    };
 
-  const content = (
-    <Component
-      ref={backdropElRef}
-      className={cn(
-        'JinniBackdrop',
-        { invisible },
-        `${theme}-theme`,
-        className
-      )}
-      onClick={handleBackdropClick}
-      style={newStyle}
-      data-disable-scroll={disableScroll}
-      {...rest}
-    >
-      {children}
-    </Component>
-  );
+    const content = (
+      <Component
+        ref={(element) => {
+          if (element) {
+            (backdropElRef as MutableRefObject<HTMLElement>).current = element;
+            if (typeof ref === 'function') {
+              ref(element);
+            } else if (ref && 'current' in ref) {
+              (ref as MutableRefObject<HTMLElement>).current = element;
+            }
+          }
+        }}
+        className={cn(
+          'JinniBackdrop',
+          { invisible },
+          `${theme}-theme`,
+          className
+        )}
+        onClick={handleBackdropClick}
+        style={newStyle}
+        data-disable-scroll={disableScroll}
+        {...rest}
+      >
+        {children}
+      </Component>
+    );
 
-  return <>{disablePortal ? content : createPortal(content, document.body)}</>;
-};
+    return (
+      <>{disablePortal ? content : createPortal(content, document.body)}</>
+    );
+  }
+);
 
 export default Backdrop;
---
+++ src/components/navigation/SwipeableDrawer/SwipeableDrawer.tsx
@@ -0,0 +1,275 @@
+import './SwipeableDrawer.scss';
+import cn from 'classnames';
+import { useId, useLayoutEffect, useRef, useCallback } from 'react';
+import { createPortal } from 'react-dom';
+import useStyle from '@/hooks/useStyle';
+import { AsType, DefaultComponentProps } from '@/types/default-component-props';
+import { Backdrop } from '@/components/feedback/Backdrop';
+import { Box, BoxProps } from '@/components/layout/Box';
+import {
+  useKeyboardAccessibility,
+  useDrawerTranslateWatcher,
+  useSwipe
+} from './SwipeableDrawer.hooks';
+import SwipeableDrawerContext from './SwipeableDrawer.contexts';
+import { TRIGGER_REGION } from './SwipeableDrawer.constants';
+import { getTranslate } from './SwipeableDrawer.utils';
+
+export type CloseReason = 'escapeKeydown' | 'backdropClick' | 'snap';
+type SnapPoint = {
+  threshold: number;
+  snapTo: number;
+};
+
+export type SwipeableDrawerProps<
+  T extends AsType = 'div',
+  P extends AsType = 'div'
+> = DefaultComponentProps<T> & {
+  children: React.ReactNode;
+  open: boolean;
+  onOpen?: (event: React.SyntheticEvent | Event) => void;
+  onClose?: (event: React.SyntheticEvent | Event, reason: CloseReason) => void;
+  snapPoints?: Array<SnapPoint>;
+  anchorOrigin?: 'left' | 'right' | 'top' | 'bottom';
+  size?: number;
+  BoxProps?: BoxProps<P>;
+};
+
+const DEFAULT_SNAP_POINTS: SnapPoint[] = [
+  { threshold: 0.5, snapTo: 0 },
+  { threshold: 1, snapTo: 1 }
+];
+
+const SwipeableDrawer = <T extends AsType = 'div', P extends AsType = 'div'>(
+  props: SwipeableDrawerProps<T, P>
+) => {
+  const {
+    children,
+    open,
+    onOpen,
+    onClose,
+    snapPoints = DEFAULT_SNAP_POINTS,
+    anchorOrigin = 'left',
+    size = 250,
+    BoxProps,
+    className,
+    style,
+    as: Component = 'div',
+    ...rest
+  } = props;
+  const drawerHeaderId = useId();
+  const drawerBodyId = useId();
+  const drawerContainerElRef = useRef<HTMLDivElement>(null);
+  const drawerElRef = useRef<HTMLElement>(null);
+  const backdropElRef = useRef<HTMLElement>(null);
+
+  const visibleDrawer = useCallback(() => {
+    const drawerContainerEl = drawerContainerElRef.current;
+    if (!drawerContainerEl) return;
+    drawerContainerEl.style.visibility = 'visible';
+  }, []);
+  const hiddenDrawer = useCallback(() => {
+    const drawerContainerEl = drawerContainerElRef.current;
+    if (!drawerContainerEl) return;
+    drawerContainerEl.style.visibility = 'hidden';
+  }, []);
+  const disableTransition = useCallback(() => {
+    const drawerEl = drawerElRef.current;
+    if (!drawerEl) return;
+    drawerEl.style.transition = 'none';
+  }, []);
+  const activeTransition = useCallback(() => {
+    const drawerEl = drawerElRef.current;
+    if (!drawerEl) return;
+    drawerEl.style.transition = open
+      ? `transform var(--jinni-duration-short4) var(--jinni-easing-standard-decelerate)`
+      : `transform var(--jinni-duration-short4) var(--jinni-easing-standard-accelerate)`;
+  }, [open]);
+  const translateDrawer = useCallback(
+    (displayedSize: number) => {
+      const drawerEl = drawerElRef.current;
+      if (!drawerEl) return;
+      switch (anchorOrigin) {
+        case 'left':
+          drawerEl.style.transform = `translateX(${-size + displayedSize}px)`;
+          break;
+        case 'right':
+          drawerEl.style.transform = `translateX(${size - displayedSize}px)`;
+          break;
+        case 'top':
+          drawerEl.style.transform = `translateY(${-size + displayedSize}px)`;
+          break;
+        case 'bottom':
+          drawerEl.style.transform = `translateY(${size - displayedSize}px)`;
+          break;
+      }
+    },
+    [size, anchorOrigin]
+  );
+  const translateDrawerByMovement = useCallback(
+    (event: PointerEvent) => {
+      const drawerEl = drawerElRef.current;
+      if (!drawerEl) return;
+      const { x, y } = getTranslate(drawerEl);
+      let displayedSize = 0;
+      switch (anchorOrigin) {
+        case 'left':
+        case 'right':
+          displayedSize = size + x + event.movementX;
+          break;
+        case 'top':
+        case 'bottom':
+          displayedSize = size + y + event.movementY;
+      }
+      translateDrawer(Math.max(Math.min(displayedSize, size), 0));
+    },
+    [anchorOrigin, size, translateDrawer]
+  );
+
+  const onProgress = useCallback((progress: number) => {
+    const backdropEl = backdropElRef.current;
+    if (!backdropEl) return;
+    backdropEl.style.opacity = `${progress}`;
+  }, []);
+  const onSwipeStart = useCallback(
+    (event: PointerEvent, trigger: boolean) => {
+      visibleDrawer();
+      disableTransition();
+      if (trigger) translateDrawer(TRIGGER_REGION);
+      else translateDrawerByMovement(event);
+    },
+    [
+      visibleDrawer,
+      translateDrawer,
+      translateDrawerByMovement,
+      disableTransition
+    ]
+  );
+  const onSwipe = useCallback(
+    (event: PointerEvent) => {
+      translateDrawerByMovement(event);
+    },
+    [translateDrawerByMovement]
+  );
+  const onSwipeEnd = useCallback(
+    (event: PointerEvent) => {
+      const drawerEl = drawerElRef.current;
+      if (!drawerEl) return;
+      activeTransition();
+      const { x, y } = getTranslate(drawerEl);
+      const displayedSize =
+        (['left', 'right'].includes(anchorOrigin) ? x : y) + size;
+      const displayRatio = displayedSize / size;
+      for (const snapPoint of snapPoints) {
+        const { threshold, snapTo } = snapPoint;
+        if (displayRatio <= threshold) {
+          if (snapTo === 1) {
+            if (open) translateDrawer(size);
+            else onOpen?.(event);
+          } else if (snapTo === 0) {
+            if (open) onClose?.(event, 'snap');
+            else translateDrawer(0);
+          } else {
+            translateDrawer(snapTo);
+          }
+          break;
+        }
+      }
+    },
+    [
+      anchorOrigin,
+      size,
+      open,
+      snapPoints,
+      activeTransition,
+      onOpen,
+      onClose,
+      translateDrawer
+    ]
+  );
+
+  const { boxElRef } = useKeyboardAccessibility({
+    open,
+    onClose
+  });
+  useDrawerTranslateWatcher({
+    ref: drawerElRef,
+    anchorOrigin,
+    maxTranslate: size,
+    onProgress
+  });
+  useSwipe({ drawerElRef, anchorOrigin, onSwipeStart, onSwipe, onSwipeEnd });
+  const newStyle = useStyle({
+    '--drawer-size': `${size}px`,
+    ...style
+  });
+
+  useLayoutEffect(() => {
+    if (open) {
+      visibleDrawer();
+      activeTransition();
+      translateDrawer(size);
+    } else {
+      const drawerEl = drawerElRef.current;
+      if (!drawerEl) return;
+      activeTransition();
+      translateDrawer(0);
+      drawerEl.addEventListener('transitionend', hiddenDrawer);
+      return () => {
+        drawerEl.removeEventListener('transitionend', hiddenDrawer);
+      };
+    }
+  }, [
+    open,
+    size,
+    visibleDrawer,
+    hiddenDrawer,
+    translateDrawer,
+    activeTransition
+  ]);
+
+  const handleBackdropClick = (e: React.MouseEvent<HTMLElement>) => {
+    onClose?.(e, 'backdropClick');
+  };
+
+  return (
+    <SwipeableDrawerContext.Provider value={{ drawerHeaderId, drawerBodyId }}>
+      {createPortal(
+        <div
+          ref={drawerContainerElRef}
+          className={cn('JinniSwipeableDrawerContainer')}
+        >
+          <Backdrop
+            ref={backdropElRef}
+            disablePortal
+            disableScroll
+            data-testid="swipeable-drawer-backdrop"
+            onClick={handleBackdropClick}
+          />
+          <Component
+            ref={drawerElRef}
+            role="dialog"
+            aria-modal={true}
+            aria-labelledby={drawerHeaderId}
+            aria-describedby={drawerBodyId}
+            className={cn('JinniSwipeableDrawer', anchorOrigin, className)}
+            style={newStyle}
+            {...rest}
+          >
+            <Box
+              ref={boxElRef}
+              className="JinniSwipeableDrawerContent"
+              elevation={15}
+              {...BoxProps}
+            >
+              {children}
+            </Box>
+          </Component>
+        </div>,
+        document.body
+      )}
+    </SwipeableDrawerContext.Provider>
+  );
+};
+
+export default SwipeableDrawer;
