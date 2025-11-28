9cfb4a807cf01ea4fae354cc6520bf3b03662102
hyejin <yhj970102@gmail.com>
2025-11-28 16:02:59 +0900

    ➖ Remove: SwitchLabel 제거

--- src/components/data-entry/SwitchLabel/SwitchLabel.tsx
+++
@@ -1,6 +0,0 @@
-import { FormControlLabel } from '@/components/_share/FormControlLabel';
-import { SwitchProps } from '@/components/data-entry/Switch';
-
-const SwitchLabel = FormControlLabel<SwitchProps>;
-
-export default SwitchLabel;
