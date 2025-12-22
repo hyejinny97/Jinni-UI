4eafe63fffe3b48c7b12aeac18d89ad5d0e54f99
hyejin <yhj970102@gmail.com>
2025-12-22 18:13:46 +0900

    📖 Storybook: ToggleButtonGroup 스토리 추가/수정

--- src/components/general/ToggleButtonGroup/ToggleButtonGroup.stories.tsx
+++ src/components/general/ToggleButtonGroup/ToggleButtonGroup.stories.tsx
@@ -3,10 +3,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 import ToggleButtonGroup from './ToggleButtonGroup';
 import { ToggleButton, ValueType } from '@/components/general/ToggleButton';
 import { Stack } from '@/components/layout/Stack';
+import { Text } from '@/components/general/Text';
 import { FormatAlignLeftIcon } from '@/components/icons/FormatAlignLeftIcon';
 import { FormatAlignCenterIcon } from '@/components/icons/FormatAlignCenterIcon';
 import { FormatAlignRightIcon } from '@/components/icons/FormatAlignRightIcon';
 import { FormatAlignJustifyIcon } from '@/components/icons/FormatAlignJustifyIcon';
+import { FormatBoldIcon } from '@/components/icons/FormatBoldIcon';
+import { FormatItalicIcon } from '@/components/icons/FormatItalicIcon';
+import { FormatUnderlinedIcon } from '@/components/icons/FormatUnderlinedIcon';
 
 const meta: Meta<typeof ToggleButtonGroup> = {
   component: ToggleButtonGroup,
@@ -14,34 +18,22 @@ const meta: Meta<typeof ToggleButtonGroup> = {
     children: {
       description: 'ToggleButton 컴포넌트들',
       table: {
-        type: { summary: 'ColorType' },
-        defaultValue: { summary: `'primary'` }
-      }
-    },
-    color: {
-      description: 'selected toggle button의 색상',
-      table: {
-        type: { summary: 'ColorType' },
-        defaultValue: { summary: `'primary'` }
+        type: { summary: 'React.ReactNode' }
       }
     },
     defaultValue: {
       description: '초기 selected value',
       table: {
-        type: { summary: `ValueType | Array<ValueType> | null` }
-      }
-    },
-    disabled: {
-      description: 'boolean',
-      table: {
-        type: { summary: `false` }
+        type: {
+          summary: `string | number | boolean | Array<string | number | boolean> | null`
+        }
       }
     },
     onChange: {
       description: 'selected value가 변경됐을 때 호출되는 함수',
       table: {
         type: {
-          summary: `(event: React.MouseEvent, value: ValueType | Array<ValueType> | null) => void`
+          summary: `(event: React.MouseEvent, value: string | number | boolean | Array<string | number | boolean> | null) => void`
         }
       }
     },
@@ -52,17 +44,12 @@ const meta: Meta<typeof ToggleButtonGroup> = {
         defaultValue: { summary: `'horizontal'` }
       }
     },
-    size: {
-      description: 'toggle button의 크기',
-      table: {
-        type: { summary: `'sm' | 'md' | 'lg'` },
-        defaultValue: { summary: `'md'` }
-      }
-    },
     value: {
       description: 'selected value',
       table: {
-        type: { summary: `ValueType | Array<ValueType> | null` }
+        type: {
+          summary: `string | number | boolean | Array<string | number | boolean> | null`
+        }
       }
     }
   }
@@ -71,24 +58,47 @@ const meta: Meta<typeof ToggleButtonGroup> = {
 export default meta;
 type Story = StoryObj<typeof ToggleButtonGroup>;
 
-const ITEMS = [
-  { value: 'left-alignment', icon: <FormatAlignLeftIcon /> },
-  { value: 'center-alignment', icon: <FormatAlignCenterIcon /> },
-  { value: 'right-alignment', icon: <FormatAlignRightIcon /> },
-  { value: 'justify-alignment', icon: <FormatAlignJustifyIcon /> }
+const ALIGNMENT_ITEMS = [
+  { value: 'left-alignment', icon: <FormatAlignLeftIcon color="gray-500" /> },
+  {
+    value: 'center-alignment',
+    icon: <FormatAlignCenterIcon color="gray-500" />
+  },
+  { value: 'right-alignment', icon: <FormatAlignRightIcon color="gray-500" /> },
+  {
+    value: 'justify-alignment',
+    icon: <FormatAlignJustifyIcon color="gray-500" />
+  }
 ];
 
-const ToggleButtonGroupTemplate = ({ ...props }) => {
-  return (
-    <ToggleButtonGroup {...props}>
-      {ITEMS.map(({ value, icon }) => (
-        <ToggleButton key={value} value={value} centerIcon={icon} />
-      ))}
-    </ToggleButtonGroup>
-  );
-};
+const FONT_ITEMS = [
+  { value: 'bold', icon: <FormatBoldIcon color="gray-500" /> },
+  {
+    value: 'italic',
+    icon: <FormatItalicIcon color="gray-500" />
+  },
+  {
+    value: 'underlined',
+    icon: <FormatUnderlinedIcon color="gray-500" />
+  }
+];
 
 const ExclusiveControlledTemplate = () => {
+  const ITEMS = [
+    { value: 'left', icon: <FormatAlignLeftIcon color="gray-500" /> },
+    {
+      value: 'center',
+      icon: <FormatAlignCenterIcon color="gray-500" />
+    },
+    {
+      value: 'right',
+      icon: <FormatAlignRightIcon color="gray-500" />
+    },
+    {
+      value: 'justify',
+      icon: <FormatAlignJustifyIcon color="gray-500" />
+    }
+  ];
   const [selectedValue, setSelectedValue] = useState<ValueType | null>(null);
 
   const handleChange = (
@@ -99,11 +109,31 @@ const ExclusiveControlledTemplate = () => {
   };
 
   return (
-    <ToggleButtonGroupTemplate value={selectedValue} onChange={handleChange} />
+    <Stack spacing={10} style={{ alignItems: 'center' }}>
+      <Text>Selected value: {String(selectedValue)}</Text>
+      <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+        {ITEMS.map(({ value, icon }) => (
+          <ToggleButton key={value} value={value}>
+            {icon}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </Stack>
   );
 };
 
 const MultipleControlledTemplate = () => {
+  const ITEMS = [
+    { value: 'bold', icon: <FormatBoldIcon color="gray-500" /> },
+    {
+      value: 'italic',
+      icon: <FormatItalicIcon color="gray-500" />
+    },
+    {
+      value: 'underlined',
+      icon: <FormatUnderlinedIcon color="gray-500" />
+    }
+  ];
   const [selectedValue, setSelectedValue] = useState<Array<ValueType>>([]);
 
   const handleChange = (
@@ -114,13 +144,36 @@ const MultipleControlledTemplate = () => {
   };
 
   return (
-    <ToggleButtonGroupTemplate value={selectedValue} onChange={handleChange} />
+    <Stack spacing={10} style={{ alignItems: 'center' }}>
+      <Text>Selected value: {selectedValue.join(', ')}</Text>
+      <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+        {ITEMS.map(({ value, icon }) => (
+          <ToggleButton key={value} value={value}>
+            {icon}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </Stack>
   );
 };
 
 const ExclusiveSelectAtLeastOneTemplate = () => {
-  const [selectedValue, setSelectedValue] =
-    useState<ValueType>('left-alignment');
+  const ITEMS = [
+    { value: 'left', icon: <FormatAlignLeftIcon color="gray-500" /> },
+    {
+      value: 'center',
+      icon: <FormatAlignCenterIcon color="gray-500" />
+    },
+    {
+      value: 'right',
+      icon: <FormatAlignRightIcon color="gray-500" />
+    },
+    {
+      value: 'justify',
+      icon: <FormatAlignJustifyIcon color="gray-500" />
+    }
+  ];
+  const [selectedValue, setSelectedValue] = useState<ValueType>('left');
 
   const handleChange = (
     _: React.MouseEvent,
@@ -130,14 +183,31 @@ const ExclusiveSelectAtLeastOneTemplate = () => {
   };
 
   return (
-    <ToggleButtonGroupTemplate value={selectedValue} onChange={handleChange} />
+    <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+      {ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   );
 };
 
 const MultipleSelectAtLeastOneTemplate = () => {
+  const ITEMS = [
+    { value: 'bold', icon: <FormatBoldIcon color="gray-500" /> },
+    {
+      value: 'italic',
+      icon: <FormatItalicIcon color="gray-500" />
+    },
+    {
+      value: 'underlined',
+      icon: <FormatUnderlinedIcon color="gray-500" />
+    }
+  ];
   const [selectedValue, setSelectedValue] = useState<Array<ValueType>>([
-    'left-alignment',
-    'right-alignment'
+    'bold',
+    'italic'
   ]);
 
   const handleChange = (
@@ -149,83 +219,320 @@ const MultipleSelectAtLeastOneTemplate = () => {
   };
 
   return (
-    <ToggleButtonGroupTemplate value={selectedValue} onChange={handleChange} />
+    <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+      {ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   );
 };
 
 export const BasicToggleButtonGroup: Story = {
-  render: (args) => <ToggleButtonGroupTemplate {...args} />
+  render: (args) => (
+    <ToggleButtonGroup {...args}>
+      {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  )
 };
 
 export const ExclusiveDefaultValue: Story = {
   render: (args) => (
-    <ToggleButtonGroupTemplate defaultValue="left-alignment" {...args} />
+    <ToggleButtonGroup defaultValue="left-alignment" {...args}>
+      {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   )
 };
 
 export const MultipleDefaultValue: Story = {
   render: (args) => (
-    <ToggleButtonGroupTemplate
-      defaultValue={['left-alignment', 'right-alignment']}
-      {...args}
-    />
+    <ToggleButtonGroup defaultValue={['bold', 'italic']} {...args}>
+      {FONT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   )
 };
 
 export const ExclusiveControlled: Story = {
-  render: (args) => <ExclusiveControlledTemplate {...args} />
+  render: () => <ExclusiveControlledTemplate />,
+  parameters: {
+    docs: {
+      source: {
+        code: `const ExclusiveControlledTemplate = () => {
+  const ITEMS = [
+    { value: 'left', icon: <FormatAlignLeftIcon color="gray-500" /> },
+    {
+      value: 'center',
+      icon: <FormatAlignCenterIcon color="gray-500" />
+    },
+    {
+      value: 'right',
+      icon: <FormatAlignRightIcon color="gray-500" />
+    },
+    {
+      value: 'justify',
+      icon: <FormatAlignJustifyIcon color="gray-500" />
+    }
+  ];
+  const [selectedValue, setSelectedValue] = useState<ValueType | null>(null);
+
+  const handleChange = (
+    _: React.MouseEvent,
+    newValue: ValueType | Array<ValueType> | null
+  ) => {
+    setSelectedValue(newValue as ValueType | null);
+  };
+
+  return (
+    <Stack spacing={10} style={{ alignItems: 'center' }}>
+      <Text>Selected value: {String(selectedValue)}</Text>
+      <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+        {ITEMS.map(({ value, icon }) => (
+          <ToggleButton key={value} value={value}>
+            {icon}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </Stack>
+  );
+};`.trim()
+      }
+    }
+  }
 };
 
 export const MultipleControlled: Story = {
-  render: (args) => <MultipleControlledTemplate {...args} />
+  render: () => <MultipleControlledTemplate />,
+  parameters: {
+    docs: {
+      source: {
+        code: `const MultipleControlledTemplate = () => {
+  const ITEMS = [
+    { value: 'bold', icon: <FormatBoldIcon color="gray-500" /> },
+    {
+      value: 'italic',
+      icon: <FormatItalicIcon color="gray-500" />
+    },
+    {
+      value: 'underlined',
+      icon: <FormatUnderlinedIcon color="gray-500" />
+    }
+  ];
+  const [selectedValue, setSelectedValue] = useState<Array<ValueType>>([]);
+
+  const handleChange = (
+    _: React.MouseEvent,
+    newValue: ValueType | Array<ValueType> | null
+  ) => {
+    setSelectedValue(newValue as Array<ValueType>);
+  };
+
+  return (
+    <Stack spacing={10} style={{ alignItems: 'center' }}>
+      <Text>Selected value: {selectedValue.join(', ')}</Text>
+      <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+        {ITEMS.map(({ value, icon }) => (
+          <ToggleButton key={value} value={value}>
+            {icon}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </Stack>
+  );
+};
+`.trim()
+      }
+    }
+  }
+};
+
+export const Orientation: Story = {
+  render: (args) => (
+    <ToggleButtonGroup orientation="vertical" {...args}>
+      {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  )
 };
 
 export const Color: Story = {
   render: (args) => (
-    <Stack spacing={20}>
-      <ToggleButtonGroupTemplate
-        defaultValue="left-alignment"
-        color="primary"
-        {...args}
-      />
-      <ToggleButtonGroupTemplate
-        defaultValue="left-alignment"
-        color="secondary"
-        {...args}
-      />
-      <ToggleButtonGroupTemplate
-        defaultValue="left-alignment"
-        color="tertiary"
-        {...args}
-      />
-    </Stack>
+    <ToggleButtonGroup defaultValue="left-alignment" color="tertiary" {...args}>
+      {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   )
 };
 
 export const Size: Story = {
   render: (args) => (
-    <Stack spacing={20}>
-      <ToggleButtonGroupTemplate size="sm" {...args} />
-      <ToggleButtonGroupTemplate size="md" {...args} />
-      <ToggleButtonGroupTemplate size="lg" {...args} />
+    <Stack spacing={20} style={{ alignItems: 'center' }}>
+      <ToggleButtonGroup size="sm" {...args}>
+        {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+          <ToggleButton key={value} value={value}>
+            {icon}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+      <ToggleButtonGroup size="md" {...args}>
+        {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+          <ToggleButton key={value} value={value}>
+            {icon}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+      <ToggleButtonGroup size="lg" {...args}>
+        {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+          <ToggleButton key={value} value={value}>
+            {icon}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
     </Stack>
   )
 };
 
-export const Disabled: Story = {
-  render: (args) => <ToggleButtonGroupTemplate disabled {...args} />
+export const OverlayEffect: Story = {
+  render: (args) => (
+    <ToggleButtonGroup disableOverlay {...args}>
+      {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  )
 };
 
-export const VerticalToggleButtonGroup: Story = {
+export const RippleEffect: Story = {
   render: (args) => (
-    <ToggleButtonGroupTemplate orientation="vertical" {...args} />
+    <ToggleButtonGroup rippleStartLocation="center" {...args}>
+      {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  )
+};
+
+export const Disabled: Story = {
+  render: (args) => (
+    <ToggleButtonGroup disabled {...args}>
+      {ALIGNMENT_ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   )
 };
 
 export const ExclusiveSelectAtLeastOne: Story = {
-  render: (args) => <ExclusiveSelectAtLeastOneTemplate {...args} />
+  render: () => <ExclusiveSelectAtLeastOneTemplate />,
+  parameters: {
+    docs: {
+      source: {
+        code: `const ExclusiveSelectAtLeastOneTemplate = () => {
+  const ITEMS = [
+    { value: 'left', icon: <FormatAlignLeftIcon color="gray-500" /> },
+    {
+      value: 'center',
+      icon: <FormatAlignCenterIcon color="gray-500" />
+    },
+    {
+      value: 'right',
+      icon: <FormatAlignRightIcon color="gray-500" />
+    },
+    {
+      value: 'justify',
+      icon: <FormatAlignJustifyIcon color="gray-500" />
+    }
+  ];
+  const [selectedValue, setSelectedValue] = useState<ValueType>('left');
+
+  const handleChange = (
+    _: React.MouseEvent,
+    newValue: ValueType | Array<ValueType> | null
+  ) => {
+    if (newValue !== null) setSelectedValue(newValue as ValueType);
+  };
+
+  return (
+    <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+      {ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  );
+};
+`.trim()
+      }
+    }
+  }
 };
 
 export const MultipleSelectAtLeastOne: Story = {
-  render: (args) => <MultipleSelectAtLeastOneTemplate {...args} />
+  render: () => <MultipleSelectAtLeastOneTemplate />,
+  parameters: {
+    docs: {
+      source: {
+        code: `const MultipleSelectAtLeastOneTemplate = () => {
+  const ITEMS = [
+    { value: 'bold', icon: <FormatBoldIcon color="gray-500" /> },
+    {
+      value: 'italic',
+      icon: <FormatItalicIcon color="gray-500" />
+    },
+    {
+      value: 'underlined',
+      icon: <FormatUnderlinedIcon color="gray-500" />
+    }
+  ];
+  const [selectedValue, setSelectedValue] = useState<Array<ValueType>>([
+    'bold',
+    'italic'
+  ]);
+
+  const handleChange = (
+    _: React.MouseEvent,
+    newValue: ValueType | Array<ValueType> | null
+  ) => {
+    if ((newValue as Array<ValueType>).length > 0)
+      setSelectedValue(newValue as Array<ValueType>);
+  };
+
+  return (
+    <ToggleButtonGroup value={selectedValue} onChange={handleChange}>
+      {ITEMS.map(({ value, icon }) => (
+        <ToggleButton key={value} value={value}>
+          {icon}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
+  );
+};`.trim()
+      }
+    }
+  }
 };
