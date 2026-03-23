f73898ee46c799105534483996c9aab760417ffb
hyejin <yhj970102@gmail.com>
2026-03-23 21:20:41 +0900

    🐛 Fix :  일부 스토리 'context value is null' 에러 해결

--- src/components/navigation/Stepper/StepDescription/StepDescription.stories.tsx
+++ src/components/navigation/Stepper/StepDescription/StepDescription.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import StepDescription from './StepDescription';
+import { StoryErrorBoundary } from '@/components/_share/StoryErrorBoundary';
 
 const meta: Meta<typeof StepDescription> = {
   component: StepDescription,
@@ -10,7 +11,14 @@ const meta: Meta<typeof StepDescription> = {
         type: { summary: `React.ReactNode` }
       }
     }
-  }
+  },
+  decorators: [
+    (Story) => (
+      <StoryErrorBoundary>
+        <Story />
+      </StoryErrorBoundary>
+    )
+  ]
 };
 
 export default meta;
