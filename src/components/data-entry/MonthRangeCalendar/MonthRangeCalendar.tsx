7da8114c54b468ceca18b03e47ba06a4e54f1cf4
hyejin <yhj970102@gmail.com>
2026-03-26 19:16:33 +0900

    ✨ Feat :  MonthRangeCalendar 컴포넌트 생성

---
+++ src/components/data-entry/MonthRangeCalendar/MonthRangeCalendar.tsx
@@ -0,0 +1,68 @@
+import './MonthRangeCalendar.scss';
+import cn from 'classnames';
+import { AsType } from '@/types/default-component-props';
+import {
+  MonthCalendar,
+  MonthCalendarProps,
+  MonthProps
+} from '@/components/data-entry/MonthCalendar';
+import { RangeType } from '@/types/date-component';
+import {
+  useSelectedDateValue,
+  useHoveredDateValue
+} from './MonthRangeCalendar.hooks';
+import { RangeMonth } from './RangeMonth';
+
+export type MonthRangeCalendarProps<T extends AsType = 'div'> = Omit<
+  MonthCalendarProps<T>,
+  'selectedDate' | 'onMonthChange' | 'renderMonth'
+> & {
+  selectedDate?: RangeType<Date | null>;
+  onSelectDate?: (newSelectedDate: RangeType<Date | null>) => void;
+  hoveredDate?: Date | null;
+  onHoverDate?: (newHoveredDate: Date | null) => void;
+};
+
+const MonthRangeCalendar = <T extends AsType = 'div'>(
+  props: MonthRangeCalendarProps<T>
+) => {
+  const {
+    selectedDate,
+    onSelectDate,
+    hoveredDate,
+    onHoverDate,
+    className,
+    ...rest
+  } = props;
+  const { selectedDateValue, handleSelect } = useSelectedDateValue({
+    selectedDate,
+    onSelectDate
+  });
+  const { hoveredDateValue, handleHover } = useHoveredDateValue({
+    hoveredDate,
+    onHoverDate
+  });
+
+  const renderRangeMonth = (yearProps: Omit<MonthProps, 'ref'>) => {
+    return (
+      <RangeMonth
+        selectedDateValue={selectedDateValue}
+        handleSelect={handleSelect}
+        hoveredDateValue={hoveredDateValue}
+        handleHover={handleHover}
+        {...yearProps}
+      />
+    );
+  };
+
+  return (
+    <MonthCalendar
+      className={cn('JinniMonthRangeCalendar', className)}
+      spacing={0}
+      renderMonth={renderRangeMonth}
+      {...rest}
+    />
+  );
+};
+
+export default MonthRangeCalendar;
