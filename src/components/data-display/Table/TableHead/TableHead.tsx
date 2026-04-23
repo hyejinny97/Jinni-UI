f35d8d7c1838c08dff93ebb5943e6a164331e949
hyejin <yhj970102@gmail.com>
2026-04-23 18:54:47 +0900

    ✨ Feat :  TableHead 컴포넌트 수정

--- src/components/data-display/Table/TableHead/TableHead.tsx
+++ src/components/data-display/Table/TableHead/TableHead.tsx
@@ -1,7 +1,9 @@
+import './TableHead.scss';
 import cn from 'classnames';
 import { AsType, DefaultComponentProps } from '@/types/default-component-props';
 import useStyle from '@/hooks/useStyle';
-import { TableHeadContext } from './Table.contexts';
+import { TableHeadContext } from './TableHead.contexts';
+import { useTable } from '../Table.hooks';
 
 type TableHeadProps<T extends AsType = 'thead'> = DefaultComponentProps<T> & {};
 
@@ -13,12 +15,17 @@ const TableHead = <T extends AsType = 'thead'>(props: TableHeadProps<T>) => {
     as: Component = 'thead',
     ...rest
   } = props;
+  const tableContext = useTable();
   const newStyle = useStyle(style);
 
   return (
     <TableHeadContext.Provider value={true}>
       <Component
-        className={cn('JinniTableHead', className)}
+        className={cn(
+          'JinniTableHead',
+          { stickyHeader: tableContext?.stickyHeader },
+          className
+        )}
         style={newStyle}
         {...rest}
       >
