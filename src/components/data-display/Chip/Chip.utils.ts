62134bc2e5e9cfe9ee714f4a96562eaf5f848d30
hyejin <yhj970102@gmail.com>
2026-05-13 13:57:49 +0900

    ✨ Feat :  Chip - color prop의 밝기에 따라 text/background/border 색상 설정

--- src/components/data-display/Chip/Chip.utils.ts
+++ src/components/data-display/Chip/Chip.utils.ts
@@ -1,6 +1,10 @@
 import type { ColorType, JinniColor } from '@/types/color';
 import type { VariantType } from './Chip';
-import { lighten } from '@/utils/colorLuminance';
+import {
+  lighten,
+  darken,
+  getCloserToWhiteOrBlack
+} from '@/utils/colorLuminance';
 
 type Props = {
   color: Exclude<ColorType, JinniColor>;
@@ -8,14 +12,16 @@ type Props = {
 };
 
 export const getColorStyle = ({ color, variant }: Props) => {
-  const lightenColor = lighten(color, 0.8);
-  const WHITE: ColorType = 'white';
+  const closerColor = getCloserToWhiteOrBlack(color);
+  const contrastColor: ColorType = closerColor === 'white' ? 'black' : 'white';
+  const subtleColor =
+    closerColor === 'white' ? darken(color, 0.8) : lighten(color, 0.8);
   const TRANSPARENT: ColorType = 'transparent';
 
   let textColor, backgroundColor, borderColor;
   switch (variant) {
     case 'filled':
-      textColor = WHITE;
+      textColor = contrastColor;
       backgroundColor = color;
       borderColor = color;
       break;
@@ -31,8 +37,8 @@ export const getColorStyle = ({ color, variant }: Props) => {
       break;
     case 'subtle-filled':
       textColor = color;
-      backgroundColor = lightenColor;
-      borderColor = lightenColor;
+      backgroundColor = subtleColor;
+      borderColor = subtleColor;
   }
   return { textColor, backgroundColor, borderColor };
 };
