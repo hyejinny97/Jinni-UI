4cc5184fbdaee9624b88cece6c816fd6818430a0
hyejin <yhj970102@gmail.com>
2026-06-18 15:02:13 +0900

    ✨ Feat : DefaultComponentProps - ref prop 타입 재정의

--- src/types/default-component-props.ts
+++ src/types/default-component-props.ts
@@ -2,15 +2,17 @@ import type { StyleType } from '@/types/style';
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export type AsType = keyof JSX.IntrinsicElements | React.ComponentType<any>;
+
 type DefaultProps<T extends AsType> = T extends keyof JSX.IntrinsicElements
   ? JSX.IntrinsicElements[T]
   : React.ComponentPropsWithoutRef<T>;
 
 export type DefaultComponentProps<T extends AsType> = Omit<
   DefaultProps<T>,
-  'style'
+  'style' | 'ref'
 > & {
   as?: T;
   className?: string;
   style?: StyleType;
+  ref?: React.Ref<React.ComponentRef<T>>;
 };
